bca6db0ad923aaef10f3c4798873fabe039ec940
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-07-25 16:56:15 -0700

    Update getVendorName.php

--- model/vendor/getVendorName.php
+++ model/vendor/getVendorName.php
@@ -1,1 +1,13 @@
+<?php
+	$venQuery = 'SELECT * FROM vendor';
+	$venStatement = $conn->prepare($venQuery);
+	$venStatement->execute();
+	
+	if($venStatement->rowCount() > 0) {
+		while($row = $venStatement->fetch(PDO::FETCH_ASSOC)) {
+			echo '<option value="' .$row['fullName'] . '">' . $row['fullName'] . '</option>';
+		}
+	}
+	$venStatement->closeCursor();
+?>
 
