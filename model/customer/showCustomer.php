a821da2e1f1885383d305b37eccc9d05720add35
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-07-25 16:27:10 -0700

    Update showCustomer.php

--- model/customer/showCustomer.php
+++ model/customer/showCustomer.php
@@ -1,1 +1,31 @@
-
+<?php
+	require_once('../../include/config/constants.php');
+	require_once('../../include/config/db.php');
+	
+	// Check if the POST request is received and if so, execute the script
+	if(isset($_POST['textBoxValue'])){
+		$output = '';
+		$custIDString = '%' . htmlentities($_POST['textBoxValue']) . '%';
+		
+		// Construct the SQL query to get the customer ID
+		$query = 'SELECT customerID FROM customer WHERE customerID LIKE ?';
+		$statement = $conn->prepare($query);
+		$statement->execute([$custIDString]);
+		
+		// If we receive any results from the above query, then display them in a list
+		if($statement->rowCount() > 0){
+			
+			// Given customer ID is available in DB. Hence create the dropdown list
+			$output = '<ul class="list-unstyled suggestionsList" id="customerDetailsCustomerIDSuggestionsList">';
+			while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+				$output .= '<li>' . $row['customerID'] . '</li>';
+			}
+			echo '</ul>';
+		} else {
+			$output = '';
+		}
+		$statement->closeCursor();
+		echo $output;
+	}
+?>
+    
