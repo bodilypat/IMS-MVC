1e996052a607cf926cc092d37d3f32987835968a
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-07-25 20:56:40 -0700

    Update resetPassword.php

--- model/login/resetPassword.php
+++ model/login/resetPassword.php
@@ -1,6 +1,6 @@
 <?php
-	require_once('../../inc/config/constants.php');
-	require_once('../../inc/config/db.php');
+	require_once('../../include/config/constants.php');
+	require_once('../../include/config/db.php');
 	
 	$resetPasswordUsername = '';
 	$resetPasswordPassword1 = '';
