3d64cf56371bbf2726074e168524bb8763a88dd1
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-07-25 18:42:04 -0700

    Update populateLastPurchase.php

--- model/purchase/populateLastPurchase.php
+++ model/purchase/populateLastPurchase.php
@@ -1,1 +1,11 @@
-
+<?php
+	require_once('../../include/config/constants.php');
+	require_once('../../include/config/db.php');
+	
+	$query = "SELECT MAX(purchaseID) FROM purchase";
+	$statement = $conn->prepare($query);
+	$statement->execute();
+	$row = $statement->fetch(PDO::FETCH_ASSOC);
+	
+	echo $row['MAX(purchaseID)'];
+?>
