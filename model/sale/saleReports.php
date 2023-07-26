09a698aa3b27dba98a6fe8987ceee7677c692a80
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-07-25 17:19:57 -0700

    Update saleReports.php

--- model/sale/saleReports.php
+++ model/sale/saleReports.php
@@ -1,1 +1,73 @@
+<?php
+	require_once('../../include/config/constants.php');
+	require_once('../../include/config/db.php');
+	
+	$unitPrice = 0;
+	$quantity = 0;
+	$totalPrice = 0;
+	
+	$saleSearchQuery = 'SELECT * FROM sale';
+	$saleSearchStatement = $conn->prepare($saleSearchQuery);
+	$saleSearchStatement->execute();
+
+	$output = '<table id="saleReportsTable" class="table table-sm table-striped table-bordered table-hover" style="width:100%">
+				<thead>
+					<tr>
+						<th>Sale ID</th>
+						<th>Item Number</th>
+						<th>Customer ID</th>
+						<th>Customer Name</th>
+						<th>Item Name</th>
+						<th>Sale Date</th>
+						<th>Discount %</th>
+						<th>Quantity</th>
+						<th>Unit Price</th>
+						<th>Total Price</th>
+					</tr>
+				</thead>
+				<tbody>';
+	
+	// Create table rows from the selected data
+	while($row = $saleSearchStatement->fetch(PDO::FETCH_ASSOC)){
+		$unitPrice = $row['unitPrice'];
+		$quantity = $row['quantity'];
+		$discount = $row['discount'];
+		$totalPrice = $unitPrice * $quantity * ((100 - $discount)/100);
+		
+		$output .= '<tr>' .
+						'<td>' . $row['saleID'] . '</td>' .
+						'<td>' . $row['itemNumber'] . '</td>' .
+						'<td>' . $row['customerID'] . '</td>' .
+						'<td>' . $row['customerName'] . '</td>' .
+						'<td>' . $row['itemName'] . '</td>' .
+						'<td>' . $row['saleDate'] . '</td>' .
+						'<td>' . $row['discount'] . '</td>' .
+						'<td>' . $row['quantity'] . '</td>' .
+						'<td>' . $row['unitPrice'] . '</td>' .
+						'<td>' . $totalPrice . '</td>' .
+					'</tr>';
+	}
+	
+	$saleSearchStatement->closeCursor();
+	
+	$output .= '</tbody>
+					<tfoot>
+						<tr>
+							<th>Total</th>
+							<th></th>
+							<th></th>
+							<th></th>
+							<th></th>
+							<th></th>
+							<th></th>
+							<th></th>
+							<th></th>
+							<th></th>
+						</tr>
+					</tfoot>
+				</table>';
+	echo $output;
+?>
+
+
 
