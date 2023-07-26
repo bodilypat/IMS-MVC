0ec89c2bbfbb094a35af7b307adca18073447878
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-07-25 19:34:23 -0700

    Update populateLastProduct.php

--- model/item/populateLastProduct.php
+++ model/item/populateLastProduct.php
@@ -1,1 +1,12 @@
-
+<?php
+	require_once('../../include/config/constants.php');
+	require_once('../../include/config/db.php');
+	
+	$query = "SELECT MAX(productID) FROM item";
+	$statement = $conn->prepare($query);
+	$statementt->execute();
+	$row = $statement->fetch(PDO::FETCH_ASSOC);
+	
+	echo $row['MAX(productID)'];
+	$statement->closeCursor();
+?>
