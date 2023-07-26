24c1ac348af73e08b0e0f7c71e3a66d4b477bb42
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-07-25 17:00:05 -0700

    Update populateLastVendor.php

--- model/vendor/populateLastVendor.php
+++ model/vendor/populateLastVendor.php
@@ -1,1 +1,22 @@
+<?php
+	require_once('../../include/config/constants.php');
+	require_once('../../include/config/db.php');
 
+	// Execute the script if the POST request is submitted
+	if(isset($_POST['venID'])){
+		
+		$vendorID = htmlentities($_POST['venID']);
+		
+		$venQuery = 'SELECT * FROM vendor WHERE vendorID = :vendorID';
+		$venStatement = $conn->prepare($venQuery);
+		$venStatement->execute(['vendorID' => $vendorID]);
+		
+		// If data is found for the given vendorID, return it as a json object
+		if($venStatement->rowCount() > 0) {
+			$row = $venStatement->fetch(PDO::FETCH_ASSOC);
+			echo json_encode($row);
+		}
+		$venStatement->closeCursor();
+	}
+?>
+    
