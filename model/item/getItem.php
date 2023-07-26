0be2394912f5ba6c9b53a5bed4a97a40f7d6b6d4
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-07-25 19:14:49 -0700

    Update getItem.php

--- model/item/getItem.php
+++ model/item/getItem.php
@@ -1,1 +1,13 @@
+<?php
+	$queryItem = 'SELECT * FROM item';
+	$itemStatement = $conn->prepare($queryItem);
+	$itemStatement->execute();
+	
+	if($itemStatement->rowCount() > 0) {
+		while($row = $itemStatement->fetch(PDO::FETCH_ASSOC)) {
+			echo '<option>' . $row['itemName'] . '</option>';
+		}
+	}
+	$itemStatement->closeCursor();
+?>
 
