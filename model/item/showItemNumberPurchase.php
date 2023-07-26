757ec115dba66878f299a3c85ad39bf6a52e80af
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-07-25 19:49:32 -0700

    Update showItemNumberPurchase.php

--- model/item/showItemNumberPurchase.php
+++ model/item/showItemNumberPurchase.php
@@ -1,1 +1,29 @@
-
+<?php
+	require_once('../../include/config/constants.php');
+	require_once('../../include/config/db.php');
+	
+	// Check if the POST request is received and if so, execute the script
+	if(isset($_POST['textBoxValue'])){
+		$output = '';
+		$itemNumString = '%' . htmlentities($_POST['textBoxValue']) . '%';
+		
+		// Construct the SQL query to get the item name
+		$query = 'SELECT itemNumber FROM item WHERE itemNumber LIKE ?';
+		$statement = $conn->prepare($query);
+		$statement->execute([$itemNumString]);
+		
+		// If we receive any results from the above query, then display them in a list
+		if($statement->rowCount() > 0){
+			$output = '<ul class="list-unstyled suggestionsList" id="purchaseDetailsItemNumberSuggestionsList">';
+			while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+				$output .= '<li>' . $row['itemNumber'] . '</li>';
+			}
+			echo '</ul>';
+		} else {
+			$output = '';
+		}
+		$statement->closeCursor();
+		echo $output;
+	}
+?>
+    
