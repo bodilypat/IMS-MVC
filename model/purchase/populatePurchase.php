944363a05cecff8243a228cacd623a3ce0ce39b7
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-07-25 18:24:53 -0700

    Update populatePurchase.php

--- model/purchase/populatePurchase.php
+++ model/purchase/populatePurchase.php
@@ -1,1 +1,21 @@
+<?php
+	require_once('../../include/config/constants.php');
+	require_once('../../include/config/db.php');
 
+	// Execute the script if the POST request is submitted
+	if(isset($_POST['purchaseID'])){
+		
+		$purchaseID = htmlentities($_POST['purchaseID']);
+		
+		$queryPurchase = 'SELECT * FROM purchase WHERE purchaseID = :purchaseID';
+		$purchaseStatement = $conn->prepare($queryPurchase);
+		$purchaseStatement->execute(['purchaseID' => $purchaseID]);
+		
+		// If data is found for the given purchaseID, return it as a json object
+		if($purchaseStatement->rowCount() > 0) {
+			$row = $purchaseStatement->fetch(PDO::FETCH_ASSOC);
+			echo json_encode($row);
+		}
+		$purchaseStatement->closeCursor();
+	}
+?>
