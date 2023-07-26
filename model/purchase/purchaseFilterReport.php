9b2247987315348dd5d7449b9dc175ebdb76c55e
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-07-25 18:48:02 -0700

    Update purchaseFilterReport.php

--- model/purchase/purchaseFilterReport.php
+++ model/purchase/purchaseFilterReport.php
@@ -1,1 +1,74 @@
+<?php
+	require_once('../../include/config/constants.php');
+	require_once('../../include/config/db.php');
+	
+	$unitPrice = 0;
+	$quantity = 0;
+	$totalPrice = 0;
+	
+	if(isset($_POST['startDate'])){
+		$startDate = htmlentities($_POST['startDate']);
+		$endDate = htmlentities($_POST['endDate']);
+		
+		$purFilterQuery = 'SELECT * FROM purchase WHERE purchaseDate BETWEEN :startDate AND :endDate';
+		$purchaseFilterStatement = $conn->prepare($purFilterQuery);
+		$purchaseFilterStatement->execute(['startDate' => $startDate, 'endDate' => $endDate]);
+
+		$output = '<table id="purchaseFilteredReportsTable" class="table table-sm table-striped table-bordered table-hover" style="width:100%">
+					<thead>
+						<tr>
+							<th>Purchase ID</th>
+							<th>Item Number</th>
+							<th>Purchase Date</th>
+							<th>Item Name</th>
+							<th>Vendor Name</th>
+							<th>Vendor ID</th>
+							<th>Quantity</th>
+							<th>Unit Price</th>
+							<th>Total Price</th>
+						</tr>
+					</thead>
+					<tbody>';
+		
+		// Create table rows from the selected data
+		while($row = $purchaseFilterStatement->fetch(PDO::FETCH_ASSOC)){
+			$unitPrice = $row['unitPrice'];
+			$quantity = $row['quantity'];
+			$totalPrice = $unitPrice * $quantity;
+		
+			$output .= '<tr>' .
+							'<td>' . $row['purchaseID'] . '</td>' .
+							'<td>' . $row['itemNumber'] . '</td>' .
+							'<td>' . $row['purchaseDate'] . '</td>' .
+							'<td>' . $row['itemName'] . '</td>' .
+							'<td>' . $row['vendorName'] . '</td>' .
+							'<td>' . $row['vendorID'] . '</td>' .
+							'<td>' . $row['quantity'] . '</td>' .
+							'<td>' . $row['unitPrice'] . '</td>' .
+							'<td>' . $totalPrice . '</td>' .
+						'</tr>';
+		}
+		
+		$purchaseFilterStatement->closeCursor();
+		
+		$output .= '</tbody>
+						<tfoot>
+							<tr>
+								<th>Total</th>
+								<th></th>
+								<th></th>
+								<th></th>
+								<th></th>
+								<th></th>
+								<th></th>
+								<th></th>
+								<th></th>
+							</tr>
+						</tfoot>
+					</table>';
+		echo $output;
+	}
+?>
+
+
 
