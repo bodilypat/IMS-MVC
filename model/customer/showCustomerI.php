b967f94bcd85f3f8bdf11f782d93c542754114e8
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-08-21 10:51:30 -0700

    Add files via upload

---
+++ model/customer/showCustomerI.php
@@ -0,0 +1,29 @@
+<?php
+	require_once('../../include/config/constants.php');
+	require_once('../../include/config/db.php');
+	
+	/* Check if the POST request is received and if so, execute the script */
+	if(isset($_POST['textBoxValue'])){
+		$output = '';
+		$custIDString = '%' . htmlentities($_POST['textBoxValue']) . '%';
+		
+		/*  Construct the SQL query to get the customer ID */
+		$qCust = 'SELECT customerID FROM customer WHERE customerID LIKE ?';
+		$custStatement = $conn->prepare($qCust);
+		$custStatement->execute([$custIDString]);
+		
+		if($custStatement->rowCount() > 0){
+			
+			/*  Given customer ID is available in DB. Hence create the dropdown list */
+			$output = '<ul class="list-unstyled suggestionsList" id="customerDetailsCustomerIDSuggestionsList">';
+			while($row = $custStatement->fetch(PDO::FETCH_ASSOC)){
+				$output .= '<li>' . $row['customerID'] . '</li>';
+			}
+			echo '</ul>';
+		} else {
+			$output = '';
+		}
+		$custStatement->closeCursor();
+		echo $output;
+	}
+?>
