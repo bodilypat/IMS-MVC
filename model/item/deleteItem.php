ab8cad4f372e368456a57abbaa4ecfa915df1e9e
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-08-21 10:55:21 -0700

    Update deleteItem.php

--- model/item/deleteItem.php
+++ model/item/deleteItem.php
@@ -6,35 +6,34 @@
 	
 	if(isset($_POST['itemNumber'])){
 		
-		// Check if mandatory fields are not empty
+		/*  Check if mandatory fields are not empty */
 		if(!empty($itemNumber)){
-			
-			// Sanitize item number
+						
 			$itemNumber = filter_var($itemNumber, FILTER_SANITIZE_STRING);
 
-			// Check if the item is in the database
-			$queryItem = 'SELECT itemNumber FROM item WHERE itemNumber=:itemNumber';
-			$itemStatement = $conn->prepare($queryItem);
+			/*  Check if the item is in the database */
+			$qItem = 'SELECT itemNumber FROM item WHERE itemNumber=:itemNumber';
+			$itemStatement = $conn->prepare($qItem);
 			$itemStatement->execute(['itemNumber' => $itemNumber]);
 			
 			if($itemStatement->rowCount() > 0){
 				
-				// Item exists in DB. Hence start the DELETE process
-				$deleteItem = 'DELETE FROM item WHERE itemNumber=:itemNumber';
-				$deleteItemStatement = $conn->prepare($deleteItem);
-				$deleteItemStatement->execute(['itemNumber' => $itemNumber]);
+				/* Item exists in DB. Hence start the DELETE process */
+				$delItem = 'DELETE FROM item WHERE itemNumber=:itemNumber';
+				$delItemStatement = $conn->prepare($delItem);
+				$delItemStatement->execute(['itemNumber' => $itemNumber]);
 
 				echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Item deleted.</div>';
 				exit();
 				
 			} else {
-				// Item does not exist, therefore, tell the user that he can't delete that item 
+				/*  Item does not exist. can't delete that item */ 
 				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Item does not exist in DB. Therefore, can\'t delete.</div>';
 				exit();
 			}
 			
 		} else {
-			// Item number is empty. Therefore, display the error message
+			/*  Item number is empty. display the error message */
 			echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter the item number</div>';
 			exit();
 		}
