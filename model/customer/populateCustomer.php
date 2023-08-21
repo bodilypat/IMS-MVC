29b11fa9c6f6886576fdac47a3e49c91ec120693
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-08-21 10:50:13 -0700

    Add files via upload

---
+++ model/customer/populateCustomer.php
@@ -0,0 +1,21 @@
+<?php
+	require_once('../../include/config/constants.php');
+	require_once('../../include/config/db.php');
+
+	/* Execute the script if the POST request is submitted */
+	if(isset($_POST['custID'])){
+		
+		$customerID = htmlentities($_POST['custID']);
+		
+		$qCust= 'SELECT * FROM customer WHERE customerID = :customerID';
+		$custStatement = $conn->prepare($qCust);
+		$custStatement->execute(['customerID' => $customerID]);
+		
+		/*  If data is found for the given item number, return it as a json object */
+		if($custStatement->rowCount() > 0) {
+			$row = $custStatement->fetch(PDO::FETCH_ASSOC);
+			echo json_encode($row);
+		}
+		$custStatement->closeCursor();
+	}
+?>
