255c89586bdd9bbab3bc21bd0833406fbe1d93c8
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-08-21 15:10:48 -0700

    Update updatePurchase.php

--- model/purchase/updatePurchase.php
+++ model/purchase/updatePurchase.php
@@ -1,9 +1,7 @@
 <?php
 
-// Updated script - 2018-05-09
-
-	require_once('../../inc/config/constants.php');
-	require_once('../../inc/config/db.php');
+	require_once('../../include/config/constants.php');
+	require_once('../../include/config/db.php');
 	
 	if(isset($_POST['purchaseDetailsPurchaseID'])){
 
@@ -21,121 +19,104 @@
 		$newStock = 0;
 		$orderItemNumber = '';
 		
-		// Check if mandatory fields are not empty
+		/*  Check if mandatory fields are not empty */
 		if(isset($purchaseItemNumber) && isset($purchaseDate) && isset($purchaseQuantity) && isset($purchaseUnitPrice)){
 			
-			// Sanitize item number
+			/* Sanitize item number */
 			$purchaseItemNumber = filter_var($purchaseItemNumber, FILTER_SANITIZE_STRING);
 			
-			// Validate item quantity. It has to be an integer
-			if(filter_var($purchaseQuantity, FILTER_VALIDATE_INT) === 0 || filter_var($purchaseQuantity, FILTER_VALIDATE_INT)){
-				// Quantity is valid
-			} else {
-				// Quantity is not a valid number
+			/*  Validate item quantity. It has to be an integer */
+			if(filter_var($purchaseQuantity, FILTER_VALIDATE_INT) === 0 || filter_var($purchaseQuantity, FILTER_VALIDATE_INT)){				
+			} else {				
 				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid number for quantity.</div>';
 				exit();
 			}
-			
-			// Validate unit price. It has to be an integer or floating point value
+
 			if(filter_var($purchaseUnitPrice, FILTER_VALIDATE_FLOAT) === 0.0 || filter_var($purchaseUnitPrice, FILTER_VALIDATE_FLOAT)){
-				// Valid unit price
-			} else {
-				// Unit price is not a valid number
+			} else {	
 				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid number for unit price.</div>';
 				exit();
 			}
 			
-			// Check if purchaseID is empty
 			if($purchaseID == ''){ 
 				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a Purchase ID.</div>';
 				exit();
 			}
 			
-			// Check if itemNumber is empty
 			if($purchaseItemNumber == ''){ 
 				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter Item Number.</div>';
 				exit();
 			}
 			
-			// Check if quantity is empty
 			if($purchaseQuantity == ''){ 
 				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter quantity.</div>';
 				exit();
 			}
 			
-			// Get the quantity and itemNumber in original purchase order
-			
-			$queryPurchase = 'SELECT * FROM purchase WHERE purchaseID = :purchaseID';
-			$purchaseStatement = $conn->prepare($queryPurchase);
+			$qPurchase = 'SELECT * FROM purchase WHERE purchaseID = :purchaseID';
+			$purchaseStatement = $conn->prepare($qPurchase);
 			$purchaseStatement->execute(['purchaseID' => $purchaseID]);
 			
-			// Get the vendorId for the given vendorName
+			/* Get the vendorId for the given vendorName */
 			
-			$queryVendor = 'SELECT * FROM vendor WHERE fullName = :fullName';
-			$vendorStatement = $conn->prepare($queryVendor);
+			$qVendor = 'SELECT * FROM vendor WHERE fullName = :fullName';
+			$vendorStatement = $conn->prepare($qVendor);
 			$vendorStatement->execute(['fullName' => $purchaseVendorName]);
+
 			$row = $vendorStatement->fetch(PDO::FETCH_ASSOC);
 			$vendorID = $row['vendorID'];
 			
-			if($PurchaseStatement->rowCount() > 0){
+			if($purchaseStatement->rowCount() > 0){
 				
-				// Purchase details exist in DB. Hence proceed to calculate the stock
-				$quantityRow = $purchaseStatement->fetch(PDO::FETCH_ASSOC);
-				$quantityOrder = $quantityRow['quantity'];
-				$orderItemNumber = $quantityRow['itemNumber'];
+				/* Purchase details exist in DB. Hence proceed to calculate the stock */
+				$resultPurch = $purchaseStatement->fetch(PDO::FETCH_ASSOC);
+				$quantityOrder = $resultPurch['quantity'];
+				$orderItemNumber = $resultPurch['itemNumber'];
 
-				// Check if the user wants to update the itemNumber too. In that case,
-				// we need to remove the quantity of the original order for that item and 
-				// update the new item details in the item table.
-				// Check if the original itemNumber is the same as the new itemNumber
+				/* 	Check if the original itemNumber is the same as the new itemNumber */
 				if($orderItemNumber !== $purchaseItemNumber) {
-					// Item numbers are different. That means the user wants to update a new item number too
-					// in that case, need to update both items' stocks.
+
+					$qItem = 'SELECT * FROM item WHERE itemNumber = :itemNumber';
+					$itemStatement = $conn->prepare($qItem);
+					$itemStatement->execute(['itemNumber' => $purchaseItemNumber]);
+					
+					if($itemStatement->rowCount() < 1){
 						
-					// Get the stock of the new item from item table
-					
-					$queryItemStock = 'SELECT * FROM item WHERE itemNumber = :itemNumber';
-					$itemCurrentStockStatement = $conn->prepare($queryItemStock);
-					$itemCurrentStockStatement->execute(['itemNumber' => $purchaseItemNumber]);
-					
-					if($itemCurrentStockStatement->rowCount() < 1){
-						// Item number is not in DB. Hence abort.
 						echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Item Number does not exist in DB. If you want to update this item, please add it to DB first.</div>';
 						exit();
 					}
 					
-					// Calculate the new stock value for new item using the existing stock in item table
+					/* Calculate the new stock value for new item using the existing stock in item table */	
+					$resultItem = $itemStatement->fetch(PDO::FETCH_ASSOC);
+					$quantityStockItem = $resultItem['stock'];
+					$quantityOrder = $purchaseQuantity;
+					$newStockItem = $quantityStockItem + $quantityOrder;
 					
-					$itemRow = $itemCurrentStockStatement->fetch(PDO::FETCH_ASSOC);
-					$quantityItem = $itemRow['stock'];
-					$quantityNewItem = $purchaseQuantity;
-					$newItemStock = $quantityItem + $quantityNewItem;
+					/*  Edit the stock for new item in item table */
 					
-					// Edit the stock for new item in item table
+					$editStock = 'UPDATE item SET stock = :stock WHERE itemNumber = :itemNumber';
+					$updateStockItemStatement = $conn->prepare($editStock);
+					$updateStockItemStatement->execute(['stock' => $newStockItem, 'itemNumber' => $purchaseItemNumber]);
 					
-					$editItemStock = 'UPDATE item SET stock = :stock WHERE itemNumber = :itemNumber';
-					$updateItemStockStatement = $conn->prepare($editItemStock);
-					$updatetemStockStatement->execute(['stock' => $newItemStock, 'itemNumber' => $purchaseItemNumber]);
+					/* Get the current stock of the previous item */
 					
-					// Get the current stock of the previous item
+					$qItem = 'SELECT * FROM item WHERE itemNumber=:itemNumber';
+					$itemStatement = $conn->prepare($qItem);
+					$itemStatement->execute(['itemNumber' => $OrderItemNumber]);
 					
-					$queryStock = 'SELECT * FROM item WHERE itemNumber=:itemNumber';
-					$itemStockStatement = $conn->prepare($queryStock);
-					$itemStockStatement->execute(['itemNumber' => $OrderItemNumber]);
+					/* Calculate the new stock value for the previous item using the existing stock in item table */
 					
-					// Calculate the new stock value for the previous item using the existing stock in item table
+					$itemInfo = $itemStatement->fetch(PDO::FETCH_ASSOC);
+					$currentStockItem = $itemInfo['stock'];
+					$newStockItem = $currentStockItem - $quantityOrder;
 					
-					$itemRow = $itemStockStatement->fetch(PDO::FETCH_ASSOC);
-					$currentQuantityItem = $itemRow['stock'];
-					$itemNewStock = $currentQuantityItem - $quantityOrder;
+					/* EDIT the stock for previous item in item table */
 					
-					// EDIT the stock for previous item in item table
+					$editStock = 'UPDATE item SET stock = :stock WHERE itemNumber = :itemNumber';
+					$updateStockItemStatement = $conn->prepare($editStock);
+					$updateStockItemStatement->execute(['stock' => $newStockItem, 'itemNumber' => $OrderItemNumber]);
 					
-					$editItemStock = 'UPDATE item SET stock = :stock WHERE itemNumber = :itemNumber';
-					$updateItemStockStatement = $conn->prepare($editItemStock);
-					$updateItemStockStatement->execute(['stock' => $itemNewStock, 'itemNumber' => $OrderItemNumber]);
-					
-					// Finally UPDATE the purchase table for new item
+					/*  Finally UPDATE the purchase table for new item */
 					
 					$updatePurchase = 'UPDATE purchase SET itemNumber = :itemNumber, purchaseDate = :purchaseDate, itemName = :itemName, unitPrice = :unitPrice, quantity = :quantity, vendorName = :vendorName, vendorID = :vendorID WHERE purchaseID = :purchaseID';
 					$updatePurchaseStatement = $conn->prepare($updatePurchase);
@@ -145,31 +126,30 @@
 					exit();
 					
 				} else {
-					// Item numbers are equal. That means item number is valid
 					
-					// Get the quantity (stock) in item table
+					/* Get the quantity (stock) in item table */
 					
-					$queryStock = 'SELECT * FROM item WHERE itemNumber=:itemNumber';
-					$stockStatement = $conn->prepare($queryStock);
-					$stockStatement->execute(['itemNumber' => $purchaseItemNumber]);
+					$qItem = 'SELECT * FROM item WHERE itemNumber=:itemNumber';
+					$itemStatement = $conn->prepare($qItem);
+					$itemStatement->execute(['itemNumber' => $purchaseItemNumber]);
 					
-					if($stockStatement->rowCount() > 0){
-						// Item exists in the item table, therefore, start inserting data to purchase table
+					if($itemStatement->rowCount() > 0){
+						/* Item exists in the item table, therefore, start inserting data to purchase table */
 						
-						// Calculate the new stock value using the existing stock in item table
+					/* 	Calculate the new stock value using the existing stock in item table */
 						
-						$row = $stockStatement->fetch(PDO::FETCH_ASSOC);
+						$row = $itemStatement->fetch(PDO::FETCH_ASSOC);
 						$quantityNewOrder = $purchaseQuantity;
-						$originalStockItem = $row['stock'];
-						$newStock = $originalStockItem + ($quantityNewOrder - $quantityOrder);
+						$stockItem = $row['stock'];
+						$newStock = $stockItem + ($quantityNewOrder - $quantityOrder);
 						
-						// Update the new stock value in item table.
+						/*  Edit the new stock value in item table. */
 						
 						$editStock = 'UPDATE item SET stock = :stock WHERE itemNumber = :itemNumber';
-						$updateStockStatement = $conn->prepare($editStock);
-						$updateStockStatement->execute(['stock' => $newStock, 'itemNumber' => $purchaseItemNumber]);
+						$updatStockItemStatement = $conn->prepare($editStock);
+						$updateStockItemStatement->execute(['stock' => $newStock, 'itemNumber' => $purchaseItemNumber]);
 						
-						// Next, update the purchase table
+						/* update the purchase table */
 						
 						$updatePurchase = 'UPDATE purchase SET purchaseDate = :purchaseDate, unitPrice = :unitPrice, quantity = :quantity, vendorName = :vendorName, vendorID = :vendorID WHERE purchaseID = :purchaseID';
 						$updatePurchaseStatement = $conn->prepare($updatePurchase);
@@ -179,8 +159,8 @@
 						exit();
 						
 					} else {
-						// Item does not exist in item table, therefore, you can't update 
-						// purchase details for it 
+						/* Item does not exist in item table, therefore, you can't update */ 
+						/*  purchase details for it */ 
 						echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Item does not exist in DB. Therefore, first enter this item to DB using the <strong>Item</strong> tab.</div>';
 						exit();
 					}	
@@ -189,7 +169,7 @@
 	
 			} else {
 				
-				// PurchaseID does not exist in purchase table, therefore, you can't update it 
+				/*  PurchaseID does not exist in purchase table,you can't update it */ 
 				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Purchase details does not exist in DB for the given Purchase ID. Therefore, can\'t update.</div>';
 				exit();
 				
