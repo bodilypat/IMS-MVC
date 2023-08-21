6ba2b53a7b0296e1a4e8850bd2ecaf874544d018
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-08-21 12:35:46 -0700

    Update and rename populatePurchase.php to popPurchase.php

--- model/purchase/popPurchase.php
+++ model/purchase/popPurchase.php
@@ -2,19 +2,19 @@
 	require_once('../../include/config/constants.php');
 	require_once('../../include/config/db.php');
 
-	// Execute the script if the POST request is submitted
+/*  Execute the script if the POST request is submitted */
 	if(isset($_POST['purchaseID'])){
 		
 		$purchaseID = htmlentities($_POST['purchaseID']);
 		
-		$queryPurchase = 'SELECT * FROM purchase WHERE purchaseID = :purchaseID';
-		$purchaseStatement = $conn->prepare($queryPurchase);
+		$qPurchase = 'SELECT * FROM purchase WHERE purchaseID = :purchaseID';
+		$purchaseStatement = $conn->prepare($qPurchase);
 		$purchaseStatement->execute(['purchaseID' => $purchaseID]);
 		
-		// If data is found for the given purchaseID, return it as a json object
+		/* If data is found for the given purchaseID, return it as a json object */
 		if($purchaseStatement->rowCount() > 0) {
-			$row = $purchaseStatement->fetch(PDO::FETCH_ASSOC);
-			echo json_encode($row);
+			$result = $purchaseStatement->fetch(PDO::FETCH_ASSOC);
+			echo json_encode($result);
 		}
 		$purchaseStatement->closeCursor();
 	}
