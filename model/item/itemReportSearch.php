82e6fa7dd72d6a438523015eef236043f2ecbf42
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-08-21 11:17:11 -0700

    Update itemReportSearch.php

--- model/item/itemReportSearch.php
+++ model/item/itemReportSearch.php
@@ -2,8 +2,8 @@
 	require_once('../../include/config/constants.php');
 	require_once('../../include/config/db.php');
 	
-	$queryItem = 'SELECT * FROM item';
-	$itemStatement = $conn->prepare($queryItem);
+	$qItem = 'SELECT * FROM item';
+	$itemStatement = $conn->prepare($qItem);
 	$itemStatement->execute();
 
 	$output = '<table id="itemReportsTable" class="table table-sm table-striped table-bordered table-hover" style="width:100%">
@@ -21,18 +21,17 @@
 				</thead>
 				<tbody>';
 	
-	// Create table rows from the selected data
-	while($row = $itemStatement->fetch(PDO::FETCH_ASSOC)){
+	/* Create table from the selected data */
+	while($resultset = $itemStatement->fetch(PDO::FETCH_ASSOC)){
 		$output .= '<tr>' .
-						'<td>' . $row['productID'] . '</td>' .
-						'<td>' . $row['itemNumber'] . '</td>' .
-						//'<td>' . $row['itemName'] . '</td>' .
-						'<td><a href="#" class="itemDetailsHover" data-toggle="popover" id="' . $row['productID'] . '">' . $row['itemName'] . '</a></td>' .
-						'<td>' . $row['discount'] . '</td>' .
-						'<td>' . $row['stock'] . '</td>' .
-						'<td>' . $row['unitPrice'] . '</td>' .
-						'<td>' . $row['status'] . '</td>' .
-						'<td>' . $row['description'] . '</td>' .
+						'<td>' . $resultset['productID'] . '</td>' .
+						'<td>' . $resultset['itemNumber'] . '</td>' .
+						'<td><a href="#" class="itemDetailsHover" data-toggle="popover" id="' . $resultset['productID'] . '">' . $resultset['itemName'] . '</a></td>' .
+						'<td>' . $resultset['discount'] . '</td>' .
+						'<td>' . $resultset['stock'] . '</td>' .
+						'<td>' . $resultset['unitPrice'] . '</td>' .
+						'<td>' . $resultset['status'] . '</td>' .
+						'<td>' . $resultset['description'] . '</td>' .
 					'</tr>';
 	}
 	
