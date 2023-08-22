0ee7923d654951a476b26a8794f2048b914edd8a
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-08-21 19:35:34 -0700

    Update showVendor.php

--- model/vendor/showVendor.php
+++ model/vendor/showVendor.php
@@ -9,22 +9,22 @@
 		
 		/*  SQL query to get the vendor ID */
 		$qVendor = 'SELECT vendorID FROM vendor WHERE vendorID LIKE ?';
-		$vendorStatement = $conn->prepare($qVendor);
-		$vendorStatement->execute([$venIDString]);
+		$getVendorStatement = $conn->prepare($qVendor);
+		$getVendorStatement->execute([$venIDString]);
 		
 		/* If we receive any results from the above query, then display them in a list */
-		if($vendorStatement->rowCount() > 0){
+		if($getVendorStatement->rowCount() > 0){
 			
 			/*  vendor ID is available in DB. create dropdown list */
 			$output = '<ul class="list-unstyled suggestionsList" id="vendorDetailsVendorIDSuggestionsList">';
-			while($resultset = $vendorStatement->fetch(PDO::FETCH_ASSOC)){
+			while($resultset = $getVendorStatement->fetch(PDO::FETCH_ASSOC)){
 				$output .= '<li>' . $resultset['vendorID'] . '</li>';
 			}
 			echo '</ul>';
 		} else {
 			$output = '';
 		}
-		$vendorStatement->closeCursor();
+		$getVendorStatement->closeCursor();
 		echo $output;
 	}
 ?>
