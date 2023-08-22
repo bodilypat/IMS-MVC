ed812c871dd160e03bc46aa1555cc93fe3e01daf
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-08-21 19:42:09 -0700

    Update vendorReports.php

--- model/vendor/vendorReports.php
+++ model/vendor/vendorReports.php
@@ -3,10 +3,10 @@
 	require_once('../../include/config/db.php');
 	
 	$qVendor = 'SELECT * FROM vendor';
-	$vendorStatement = $conn->prepare($qVendor);
-	$vendorStatement->execute();
+	$getVendorStatement = $conn->prepare($qVendor);
+	$getVendorStatement->execute();
 
-	$output = '<table id="vendorDetailsTable" class="table table-sm table-striped table-bordered table-hover" style="width:100%">
+	$output = '<table id="vendorReportsTable" class="table table-sm table-striped table-bordered table-hover" style="width:100%">
 				<thead>
 					<tr>
 						<th>Vendor ID</th>
@@ -23,7 +23,7 @@
 				</thead>
 				<tbody>';
 	
-	/* Create table rows from the selected data */
+	// Create table rows from the selected data
 	while($resultset = $vendorStatement->fetch(PDO::FETCH_ASSOC)){
 		$output .= '<tr>' .
 						'<td>' . $resultset['vendorID'] . '</td>' .
