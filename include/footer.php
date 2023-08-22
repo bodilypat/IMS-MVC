10919864df1f59e23cc95b69d75371b653a2acba
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-08-21 21:05:21 -0700

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -6,22 +6,22 @@
     </footer>
 
     <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
 	
 	<!-- Datatables script -->
-	<script type="text/javascript" charset="utf8" src="vendor/DataTables/datatables.js"></script>
-	<script type="text/javascript" charset="utf8" src="vendor/DataTables/sumsum.js"></script>
+	<script type="text/javascript" charset="utf8" src="assets/js/datatables.js"></script>
+	<script type="text/javascript" charset="utf8" src="assets/js/sumsum.js"></script>
 	
 	<!-- Chosen files for select boxes -->
-	<script src="vendor/chosen/chosen.jquery.min.js"></script>
-	<link rel="stylesheet" href="vendor/chosen/chosen.css" />
+	<script src="assets/js/chosen.jquery.min.js"></script>
+	<link rel="stylesheet" href="assets/css/chosen.css" />
 	
 	<!-- Datepicker JS -->
-	<script src="vendor/datepicker164/js/bootstrap-datepicker.min.js"></script>
+	<script src="assets/js/bootstrap-datepicker.min.js"></script>
 	
 	<!-- Bootbox JS -->
-	<script src="vendor/bootbox/bootbox.min.js"></script>
+	<script src="assets/js/bootbox.min.js"></script>
 	
 	<!-- Custom scripts -->
 	<script src="assets/js/scripts.js"></script>
