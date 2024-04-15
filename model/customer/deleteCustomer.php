5748d7708b1eeacddb1467e984259c7da0c86ffc
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-15 15:47:11 -0700

    Update deleteCustomer.php

--- model/customer/deleteCustomer.php
+++ model/customer/deleteCustomer.php
@@ -1,28 +1,28 @@
 <?php
 	require_once('../../include/config/constants.php');
-	require_once('../../include/config/db.php');
+	require_once('../../include/config/dbconnect.php');
 	
-	if(isset($_POST['custID'])){
+	if(isset($_POST['customerID'])){
 		
-		$customerID = htmlentities($_POST['custID']);
+		$custID = htmlentities($_POST['customerID']);
 		
 		/*  Check if mandatory fields are not empty */
-		if(!empty($customerD)){
+		if(!empty($custID)){
 			
 			/*  Sanitize customerID */
-			$customerID = filter_var($customerID, FILTER_SANITIZE_STRING);
+			$custID = filter_var($custID, FILTER_SANITIZE_STRING);
 
 			/*  Check if the customer is in the database */
-			$qCust = 'SELECT customerID FROM customer WHERE customerID=:customerID';
-			$custStatement = $conn->prepare($qCust);
-			$custStatement->execute(['customerID' => $customerID]);
+			$qCust = 'SELECT customerID FROM customer WHERE customerID=:custID';
+			$custStatement = $dbcon->prepare($qCust);
+			$custStatement->execute(['customerID' => $custID]);
 			
 			if($custStatement->rowCount() > 0){
 				
 				/* Customer exists in DB. Hence start the DELETE process */
-				$delCust = 'DELETE FROM customer WHERE customerID=:customerID';
-				$delCustStatement = $conn->prepare($delCust);
-				$delCustStatement->execute(['customerID' => $customerID]);
+				$delCust = 'DELETE FROM customer WHERE customerID=:custID';
+				$custStatement = $dbcon->prepare($delCust);
+				$custStatement->execute(['customerID' => $custID]);
 
 				echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Customer deleted.</div>';
 				exit();
