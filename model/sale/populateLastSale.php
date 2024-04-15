8b2e728dcb03cde9f6aeb0b290b2ac32294e89c0
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-15 14:44:19 -0700

    Update populateLastSale.php

--- model/sale/populateLastSale.php
+++ model/sale/populateLastSale.php
@@ -1,11 +1,11 @@
 <?php
 	require_once('../../include/config/constants.php');
-	require_once('../../include/config/db.php');
+	require_once('../../include/config/dbconnect.php');
 	
-	$saleQuery = "SELECT MAX(saleID) FROM sale";
-	$statement = $conn->prepare($sql);
-	$statement->execute();
-	$row = $statement->fetch(PDO::FETCH_ASSOC);
+	$qSale = "SELECT MAX(saleID) FROM sale";
+	$saleStatement = $dbcon->prepare($qSale);
+	$saleStatement->execute();
+	$result = $saleStatement->fetch(PDO::FETCH_ASSOC);
 	
 	echo $row['MAX(saleID)'];
 ?>
