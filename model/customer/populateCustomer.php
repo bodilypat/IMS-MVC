98198ee7d8181472a544f17f52fbdc7f00a45a9d
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-15 14:48:03 -0700

    Update populateCustomer.php

--- model/customer/populateCustomer.php
+++ model/customer/populateCustomer.php
@@ -1,19 +1,19 @@
 <?php
 	require_once('../../include/config/constants.php');
-	require_once('../../include/config/db.php');
+	require_once('../../include/config/dbconnect.php');
 
 	/* Execute the script if the POST request is submitted */
-	if(isset($_POST['custID'])){
+	if(isset($_POST['customerID'])){
 		
-		$customerID = htmlentities($_POST['custID']);
+		$customerID = htmlentities($_POST['customerID']);
 		
 		$qCust= 'SELECT * FROM customer WHERE customerID = :customerID';
-		$custStatement = $conn->prepare($qCust);
+		$custStatement = $dbcon->prepare($qCust);
 		$custStatement->execute(['customerID' => $customerID]);
 		
 		/*  If data is found for the given item number, return it as a json object */
 		if($custStatement->rowCount() > 0) {
-			$row = $custStatement->fetch(PDO::FETCH_ASSOC);
+			$recordCust = $custStatement->fetch(PDO::FETCH_ASSOC);
 			echo json_encode($row);
 		}
 		$custStatement->closeCursor();
