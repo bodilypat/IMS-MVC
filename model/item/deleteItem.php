b22b36361a48701ddb9ceb71a8770e4d73457815
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-15 15:49:42 -0700

    Update deleteItem.php

--- model/item/deleteItem.php
+++ model/item/deleteItem.php
@@ -1,27 +1,27 @@
 <?php
 	require_once('../../include/config/constants.php');
-	require_once('../../include/config/db.php');
+	require_once('../../include/config/dbconnect.php');
 	
-	$itemNumber = htmlentities($_POST['itemNumber']);
+	$itemNum = htmlentities($_POST['itemNumber']);
 	
-	if(isset($_POST['itemNumber'])){
+	if(isset($_POST['itemNum'])){
 		
 		/*  Check if mandatory fields are not empty */
-		if(!empty($itemNumber)){
+		if(!empty($itemNum)){
 						
-			$itemNumber = filter_var($itemNumber, FILTER_SANITIZE_STRING);
+			$itemNum = filter_var($itemNum, FILTER_SANITIZE_STRING);
 
 			/*  Check if the item is in the database */
-			$qItem = 'SELECT itemNumber FROM item WHERE itemNumber=:itemNumber';
+			$qItem = 'SELECT itemNumber FROM item WHERE itemNumber=:itemNum';
 			$itemStatement = $conn->prepare($qItem);
-			$itemStatement->execute(['itemNumber' => $itemNumber]);
+			$itemStatement->execute(['itemNumber' => $itemNum]);
 			
 			if($itemStatement->rowCount() > 0){
 				
 				/* Item exists in DB. Hence start the DELETE process */
-				$delItem = 'DELETE FROM item WHERE itemNumber=:itemNumber';
-				$delItemStatement = $conn->prepare($delItem);
-				$delItemStatement->execute(['itemNumber' => $itemNumber]);
+				$delItem = 'DELETE FROM item WHERE itemNumber=:itemNum';
+				$itemStatement = $dbcon->prepare($delItem);
+				$itemStatement->execute(['itemNumber' => $itemNum]);
 
 				echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Item deleted.</div>';
 				exit();
