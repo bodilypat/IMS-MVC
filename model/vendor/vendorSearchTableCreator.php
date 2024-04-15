9aa15086fe412bc43f34243d7b51876012a0f04e
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-15 14:25:52 -0700

    Add files via upload

---
+++ model/vendor/vendorSearchTableCreator.php
@@ -0,0 +1,61 @@
+<?php
+	require_once('../../include/config/constants.php');
+	require_once('../../include/config/db.php');
+	
+	$qVen = 'SELECT * FROM vendor';
+	$venStatement = $dbcon->prepare($qVen);
+	$venStatement->execute();
+
+	$output = '<table id="vendorTable" class="table table-sm table-striped table-bordered table-hover" style="width:100%">
+				<thead>
+					<tr>
+						<th>Vendor ID</th>
+						<th>Full Name</th>
+						<th>Email</th>
+						<th>Mobile</th>
+						<th>Phone 2</th>
+						<th>Address</th>
+						<th>Address 2</th>
+						<th>City</th>
+						<th>District</th>
+						<th>Status</th>
+					</tr>
+				</thead>
+				<tbody>';
+	
+	// Create table rows from the selected data
+	while($resultset = $venStatement->fetch(PDO::FETCH_ASSOC)){
+		$output .= '<tr>' .
+						'<td>' . $resultset['vendorID'] . '</td>' .
+						'<td>' . $resultset['fullName'] . '</td>' .
+						'<td>' . $resultset['email'] . '</td>' .
+						'<td>' . $resultset['mobile'] . '</td>' .
+						'<td>' . $resultset['phone2'] . '</td>' .
+						'<td>' . $resultset['address'] . '</td>' .
+						'<td>' . $resultset['address2'] . '</td>' .
+						'<td>' . $resultset['city'] . '</td>' .
+						'<td>' . $resultset['district'] . '</td>' .
+						'<td>' . $resultset['status'] . '</td>' .
+					'</tr>';
+	}
+	
+	$venStatement->closeCursor();
+	
+	$output .= '</tbody>
+					<tfoot>
+						<tr>
+							<th>Vendor ID</th>
+							<th>Full Name</th>
+							<th>Email</th>
+							<th>Mobile</th>
+							<th>Phone 2</th>
+							<th>Address</th>
+							<th>Address 2</th>
+							<th>City</th>
+							<th>District</th>
+							<th>Status</th>
+						</tr>
+					</tfoot>
+				</table>';
+	echo $output;
+?>
