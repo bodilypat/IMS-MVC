eb0a696d53a960ba4959a124d69330c88161a942
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-15 14:38:40 -0700

    Update popVendor.php

--- model/vendor/popVendor.php
+++ model/vendor/popVendor.php
@@ -1,21 +1,21 @@
 <?php
 	require_once('../../include/config/constants.php');
-	require_once('../../include/config/db.php');
+	require_once('../../include/config/dbconnect.php');
 
 	/* Execute the script if the POST request is submitted */
 	if(isset($_POST['venID'])){
 		
 		$vendorID = htmlentities($_POST['venID']);
 		
-		$qVendor = 'SELECT * FROM vendor WHERE vendorID = :vendorID';
-		$getVendorStatement = $conn->prepare($qVendor);
-		$getVendorStatement->execute(['vendorID' => $vendorID]);
+		$qVen = 'SELECT * FROM vendor WHERE vendorID = :vendorID';
+		$venStatement = $dbcon->prepare($qVendor);
+		$venStatement->execute(['vendorID' => $vendorID]);
 		
 		/* If data is found for the given vendorID, return it as a json object */
-		if($getVendorStatement->rowCount() > 0) {
-			$result = $vendorStatement->fetch(PDO::FETCH_ASSOC);
+		if($venStatement->rowCount() > 0) {
+			$result = $venStatement->fetch(PDO::FETCH_ASSOC);
 			echo json_encode($result);
 		}
-		$vendorStatement->closeCursor();
+		$venStatement->closeCursor();
 	}
 ?>
