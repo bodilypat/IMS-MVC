3eda5e8fc3ec42e9babfc4a07a7b082e717d7776
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-15 13:17:43 -0700

    Add files via upload

---
+++ model/customer/custotmerSearchTableCreator.php
@@ -0,0 +1,61 @@
+<?php
+	require_once('../../include/config/constants.php');
+	require_once('../../include/config/dbconnect.php');
+	
+	$qCust = 'SELECT * FROM customer';
+	$custStatement = $dbcon->prepare($qCust);
+	$custStatement->execute();
+
+	$output = '<table id="customerSearchTable" class="table table-sm table-striped table-bordered table-hover" style="width:100%">
+				<thead>
+					<tr>
+						<th>Customer ID</th>
+						<th>Full Name</th>
+						<th>Email</th>
+						<th>Mobile</th>
+						<th>Phone 2</th>
+						<th>Address</th>
+						<th>Address 2</th>
+						<th>City</th>
+						<th>District</th>
+						<th>Status</th>
+					</tr>
+				</thead>
+				<tbody>';
+	
+	/*  Create table rows from the selected data */
+	while($resultset = $custStatement->fetch(PDO::FETCH_ASSOC)){
+		$output .= '<tr>' .
+						'<td>' . $resultset['customerID'] . '</td>' .
+						'<td>' . $resultset['fullName'] . '</td>' .
+						'<td>' . $resultset['email'] . '</td>' .
+						'<td>' . $resultset['mobile'] . '</td>' .
+						'<td>' . $resultset['phone2'] . '</td>' .
+						'<td>' . $resultset['address'] . '</td>' .
+						'<td>' . $resultset['address2'] . '</td>' .
+						'<td>' . $resultset['city'] . '</td>' .
+						'<td>' . $resultset['district'] . '</td>' .
+						'<td>' . $resultset['status'] . '</td>' .
+					'</tr>';
+	}
+	
+	$custStatement->closeCursor();
+	
+	$output .= '</tbody>
+					<tfoot>
+						<tr>
+							<th>Customer ID</th>
+							<th>Full Name</th>
+							<th>Email</th>
+							<th>Mobile</th>
+							<th>Phone 2</th>
+							<th>Address</th>
+							<th>Address 2</th>
+							<th>City</th>
+							<th>District</th>
+							<th>Status</th>
+						</tr>
+					</tfoot>
+				</table>';
+	echo $output;
+?>
