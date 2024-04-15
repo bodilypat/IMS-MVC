c87ba344a3986a9e2376bd8f842d57786c71cc50
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-15 15:07:13 -0700

    Update showPurchase.php

--- model/purchase/showPurchase.php
+++ model/purchase/showPurchase.php
@@ -1,6 +1,6 @@
 <?php
 	require_once('../../inc/config/constants.php');
-	require_once('../../inc/config/db.php');
+	require_once('../../inc/config/dbconnect.php');
 	
 	/*  Check POST request is received , execute the script */
 	if(isset($_POST['textBoxValue'])){
@@ -8,17 +8,17 @@
 		$purString = '%' . htmlentities($_POST['textBoxValue']) . '%';
 		
 		/*  get the purchase ID */
- 		$qPurchID = 'SELECT purchaseID FROM purchase WHERE purchaseID LIKE ?';
-		$purchaseStatement = $conn->prepare($qPurchID);
-		$purchaseStatement->execute([$purString]);
+ 		$qPurch = 'SELECT purchaseID FROM purchase WHERE purchaseID LIKE ?';
+		$purchStatement = $conn->prepare($qPurch);
+		$purchStatement->execute([$purString]);
 		
 		/* receive any results from  query, then display them in a list */
-		if($purchaseStatement->rowCount() > 0){
+		if($purchStatement->rowCount() > 0){
 			
 			/*  Given purchase ID is available in DB. create the dropdown list */
-			$output = '<ul class="list-unstyled suggestionsList" id="purchaseDetailsPurchaseIDSuggestionsList">';
-			while($resultset = $purchaseStatement->fetch(PDO::FETCH_ASSOC)){
-				$output .= '<li>' . $resultset['purchaseID'] . '</li>';
+			$output = '<ul class="list-unstyled adviseList" id="purchaseIDAdviseList">';
+			while($resultPur = $purchStatement->fetch(PDO::FETCH_ASSOC)){
+				$output .= '<li>' . $resultPur['purchaseID'] . '</li>';
 			}
 			echo '</ul>';
 		} else {
