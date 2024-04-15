dd71b110ac449d7480d2427f8f576c0d1edc7c35
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-15 14:42:15 -0700

    Update popLastPurchase.php

--- model/purchase/popLastPurchase.php
+++ model/purchase/popLastPurchase.php
@@ -1,11 +1,11 @@
 <?php
 	require_once('../../include/config/constants.php');
-	require_once('../../include/config/db.php');
+	require_once('../../include/config/dbconnect.php');
 	
-	$qPurchase = "SELECT MAX(purchaseID) FROM purchase";
-	$purchaseStatement = $conn->prepare($qPurchase);
-	$purchaseStatement->execute();
-	$result = $purchaseStatement->fetch(PDO::FETCH_ASSOC);
+	$qPurch = "SELECT MAX(purchaseID) FROM purchase";
+	$purchStatement = $conn->prepare($qPurch);
+	$purchStatement->execute();
+	$result = $purchStatement->fetch(PDO::FETCH_ASSOC);
 	
 	echo $result['MAX(purchaseID)'];
 ?>
