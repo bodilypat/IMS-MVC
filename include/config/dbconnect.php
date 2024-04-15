c6e1219d3c37a7a7625df80a8cc081bfecc5ef97
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-15 13:09:38 -0700

    Update dbconnect.php

--- include/config/dbconnect.php
+++ include/config/dbconnect.php
@@ -1,8 +1,8 @@
 <?php
 	// Connect to database
 	try{
-		$conn = new PDO(DSN, DB_USER, DB_PASSWORD);
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$dbcon = new PDO(DSN, DB_USER, DB_PASSWORD);
+		$dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	} catch(PDOException $e){
 		$errorMessage = $e->getMessage();
 		echo $errorMessage;
