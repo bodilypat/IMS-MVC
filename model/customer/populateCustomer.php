12da6ef4dc2061933021b766e5ab84d2ca7fb10c
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-15 15:01:29 -0700

    Update populateCustomer.php

--- model/customer/populateCustomer.php
+++ model/customer/populateCustomer.php
@@ -5,16 +5,16 @@
 	/* Execute the script if the POST request is submitted */
 	if(isset($_POST['customerID'])){
 		
-		$customerID = htmlentities($_POST['customerID']);
+		$custID = htmlentities($_POST['customerID']);
 		
-		$qCust= 'SELECT * FROM customer WHERE customerID = :customerID';
+		$qCust= 'SELECT * FROM customer WHERE customerID = :custID';
 		$custStatement = $dbcon->prepare($qCust);
-		$custStatement->execute(['customerID' => $customerID]);
+		$custStatement->execute(['customerID' => $custID]);
 		
 		/*  If data is found for the given item number, return it as a json object */
 		if($custStatement->rowCount() > 0) {
 			$recordCust = $custStatement->fetch(PDO::FETCH_ASSOC);
-			echo json_encode($row);
+			echo json_encode($recordCust);
 		}
 		$custStatement->closeCursor();
 	}
