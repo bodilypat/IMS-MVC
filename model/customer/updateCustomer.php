2f334a7a5be1dac4baa2e47c5a3b71456a6a5ef5
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-15 09:20:06 -0700

    Update updateCustomer.php

--- model/customer/updateCustomer.php
+++ model/customer/updateCustomer.php
@@ -1,84 +1,116 @@
 <?php
 	require_once('../../include/config/constants.php');
-	require_once('../../include/config/db.php');
+	require_once('../../include/config/dbconnect.php');
 	
 	/* Check if the POST query is received */
 	if(isset($_POST['customerDetailsCustomerID'])) {
 		
-		$customerID = htmlentities($_POST['custID']);
-		$customerFullName = htmlentities($_POST['custFullName']);
-		$customerMobile = htmlentities($_POST['custMobile']);
-		$customerPhone2 = htmlentities($_POST['custPhone2']);
-		$customerEmail = htmlentities($_POST['custEmail']);
-		$customerAddress = htmlentities($_POST['custAddress']);
-		$customerAddress2 = htmlentities($_POST['custAddress2']);
-		$customerCity = htmlentities($_POST['custCity']);
-		$customerDistrict = htmlentities($_POST['custDistrict']);
-		$customerStatus = htmlentities($_POST['custStatus']);
+		$custID = htmlentities($_POST['custID']);
+		$custFullName = htmlentities($_POST['custFullName']);
+		$custMobile = htmlentities($_POST['custMobile']);
+		$custPhone = htmlentities($_POST['custPhone']);
+		$custEmail = htmlentities($_POST['custEmail']);
+		$custAddress = htmlentities($_POST['custAddress']);
+		$custAddress2 = htmlentities($_POST['custAddress2']);
+		$custCity = htmlentities($_POST['custCity']);
+		$custDistrict = htmlentities($_POST['custDistrict']);
+		$custStatus = htmlentities($_POST['custStatus']);
 		
 		/*  Check if mandatory fields are not empty */
 
-		if(isset($customerFullName) && isset($customerMobile) && isset($customerAddress)) {
+		if(isset($custFullName) && isset($custMobile) && isset($custAddress)) {
 			
 		
-			if(filter_var($customerMobile, FILTER_VALIDATE_INT) === 0 || filter_var($customerMobile, FILTER_VALIDATE_INT)) {
+			if(filter_var($custMobile, FILTER_VALIDATE_INT) === 0 || filter_var($custMobile, FILTER_VALIDATE_INT)) {
 				
 			} else {
 				
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid mobile number</div>';
+				echo '<div class="alert alert-danger">
+				           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid mobile number
+					  </div>';
 				exit();
 			}
 			
-			if(empty($custCustomerID)){
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter the CustomerID to update that customer.</div>';
+			if(empty($custID)){
+				echo '<div class="alert alert-danger">
+				           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter the CustomerID to update that customer.
+				      </div>';
 				exit();
 			}
 			
-			if(!empty($custPhone2)){
-				if(filter_var($customerPhone2, FILTER_VALIDATE_INT) === 0 || filter_var($customerPhone2, FILTER_VALIDATE_INT)) {
+			if(!empty($custPhone)){
+				if(filter_var($custPhone, FILTER_VALIDATE_INT) === 0 || filter_var($custPhone, FILTER_VALIDATE_INT)) {
 					
 				} else {
 					
-					echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid number for phone number 2.</div>';
+					echo '<div class="alert alert-danger">
+					           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid number for phone number 2.
+						  </div>';
 					exit();
 				}
 			}
 			
-			if(!empty($customerEmail)) {
-				if (filter_var($customerEmail, FILTER_VALIDATE_EMAIL) === false) {
+			if(!empty($custEmail)) {
+				if (filter_var($custEmail, FILTER_VALIDATE_EMAIL) === false) {
 			
-					echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid email</div>';
+					echo '<div class="alert alert-danger">
+					           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid email
+						  </div>';
 					exit();
 				}
 			}
 
 			$qCust = 'SELECT customerID FROM customer WHERE customerID = :customerID';
-			$custStatement = $conn->prepare($qCust);
-			$custStatement->execute(['customerDetailsCustomerID' => $customerID]);
+			$custStatement = $dbcon->prepare($qCust);
+			$custStatement->execute(['customerID' => $custID]);
 			
 			if($custStatement->rowCount() > 0) {
 				
 				/*  Construct the UPDATE query */
-				$updateCust = 'UPDATE customer SET fullName = :fullName, email = :email, mobile = :mobile, phone2 = :phone2, address = :address, address2 = :address2, city = :city, district = :district, status = :status WHERE customerID = :customerID';
-				$updateCustStatement = $conn->prepare($updateCust);
-				$updateCustStatement->execute(['fullName' => $customerFullName, 'email' => $customerEmail, 'mobile' => $customerMobile, 'phone2' => $customerPhone2, 'address' => $customerAddress, 'address2' => $customerAddress2, 'city' => $customerCity, 'district' => $customerDistrict, 'status' => $customerStatus, 'customerID' => $customerID]);
+				$editCust = 'UPDATE customer SET fullName = :fullName, 
+				                                 email = :email, 
+												 mobile = :mobile, 
+												 phone2 = :phone2, 
+												 address = :address, 
+												 address2 = :address2, 
+												 city = :city, 
+												 district = :district, 
+												 status = :status 
+											WHERE customerID = :custID';
+				$custStatement = $dbcon->prepare($editCust);
+				$custStatement->execute(['fullName' => $custFullName, 
+				                          'email' => $custEmail, 
+										  'mobile' => $custMobile, 
+										  'phone2' => $custPhone2, 
+										  'address' => $custAddress, 
+										  'address2' => $custAddress2, 
+										  'city' => $custCity, 
+										  'district' => $custDistrict, 
+										  'status' => $custStatus, 
+										  'customerID' => $custID]);
 				
 				/*  EDIT customer name in sale table too */
-				$editSale = 'UPDATE sale SET customerName = :customerName WHERE customerID = :customerID';
-				$editSaleStatement = $conn->prepare($editSale);
-				$editSaleStatement->execute(['customerName' => $customerFullName, 'customerID' => $customerID]);
+				$editSale = 'UPDATE sale SET customerName = :custName WHERE customerID = :custID';
+				$saleStatement = $dbcon->prepare($editSale);
+				$saleStatement->execute(['customerName' => $custFullName, 'customerID' => $custID]);
 				
-				echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Customer details updated.</div>';
+				echo '<div class="alert alert-success">
+				           <button type="button" class="close" data-dismiss="alert">&times;</button>Customer details updated.
+					  </div>';
 				exit();
 			} else {
 				/* CustomerID is not in DB. Therefore, stop the update and quit */
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>CustomerID does not exist in DB. Therefore, update not possible.</div>';
+				echo '<div class="alert alert-danger">
+				           <button type="button" class="close" data-dismiss="alert">&times;</button>CustomerID does not exist in DB. Therefore, update not possible.
+					  </div>';
 				exit();
 			}
 			
 		} else {
 			/* One or more mandatory fields are empty. Therefore, display the error message */
-			echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter all fields marked with a (*)</div>';
+			echo '<div class="alert alert-danger">
+			           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter all fields marked with a (*)
+				  </div>';
 			exit();
 		}
 	}
