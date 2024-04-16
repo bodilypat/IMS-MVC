3979224bb877632d218e37acbe5f2eb9b6ad714a
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-15 18:00:46 -0700

    Update and rename showSale.php to showSaleID.php

--- model/sale/showSaleID.php
+++ model/sale/showSaleID.php
@@ -1,6 +1,6 @@
 <?php
 	require_once('../../include/config/constants.php');
-	require_once('../../include/config/db.php');
+	require_once('../../include/config/dbconnect.php');
 	
 	// Check if the POST request is received and if so, execute the script
 	if(isset($_POST['textBoxValue'])){
@@ -8,17 +8,17 @@
 		$saleIDString = '%' . htmlentities($_POST['textBoxValue']) . '%';
 		
 		// Construct the SQL query to get the saleID
-		$query = 'SELECT saleID FROM sale WHERE saleID LIKE ?';
-		$statement = $conn->prepare($query);
-		$statement->execute([$saleIDString]);
+		$qSale = 'SELECT saleID FROM sale WHERE saleID LIKE ?';
+		$saleStatement = $dbcon->prepare($qSale);
+		$saleStatement->execute([$saleIDString]);
 		
 		// If we receive any results from the above query, then display them in a list
 		if($statetment->rowCount() > 0){
 			
 			// Given sale ID is available in DB. Hence create the dropdown list
-			$output = '<ul class="list-unstyled suggestionsList" id="saleDetailsSaleIDSuggestionsList">';
-			while($row = $statement->fetch(PDO::FETCH_ASSOC)){
-				$output .= '<li>' . $row['saleID'] . '</li>';
+			$output = '<ul class="list-unstyled adviseList" id="saleIDAdviseList">';
+			while($result = $statement->fetch(PDO::FETCH_ASSOC)){
+				$output .= '<li>' . $result['saleID'] . '</li>';
 			}
 			echo '</ul>';
 		} else {
