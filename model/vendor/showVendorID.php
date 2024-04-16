55e22f38f9f7ce21218e1bcf8372d0c53bc86158
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-15 18:03:24 -0700

    Update and rename showVendor.php to showVendorID.php

--- model/vendor/showVendorID.php
+++ model/vendor/showVendorID.php
@@ -1,6 +1,6 @@
 <?php
 	require_once('../../include/config/constants.php');
-	require_once('../../include/config/db.php');
+	require_once('../../include/config/dbconnect.php');
 	
 	/* Check if the POST request is received , execute the script */
 	if(isset($_POST['textBoxValue'])){
@@ -8,17 +8,17 @@
 		$venIDString = '%' . htmlentities($_POST['textBoxValue']) . '%';
 		
 		/*  SQL query to get the vendor ID */
-		$qVendor = 'SELECT vendorID FROM vendor WHERE vendorID LIKE ?';
-		$getVendorStatement = $conn->prepare($qVendor);
-		$getVendorStatement->execute([$venIDString]);
+		$qVen = 'SELECT vendorID FROM vendor WHERE vendorID LIKE ?';
+		$venStatement = $dbcon->prepare($qVen);
+		$venStatement->execute([$venIDString]);
 		
 		/* If we receive any results from the above query, then display them in a list */
-		if($getVendorStatement->rowCount() > 0){
+		if($venStatement->rowCount() > 0){
 			
 			/*  vendor ID is available in DB. create dropdown list */
-			$output = '<ul class="list-unstyled suggestionsList" id="vendorDetailsVendorIDSuggestionsList">';
-			while($resultset = $getVendorStatement->fetch(PDO::FETCH_ASSOC)){
-				$output .= '<li>' . $resultset['vendorID'] . '</li>';
+			$output = '<ul class="list-unstyled adviseList" id="vendorIDAdviseList">';
+			while($result = $getVendorStatement->fetch(PDO::FETCH_ASSOC)){
+				$output .= '<li>' . $result['vendorID'] . '</li>';
 			}
 			echo '</ul>';
 		} else {
