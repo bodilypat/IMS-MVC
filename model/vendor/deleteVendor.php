e4544c60a2348987e453f606efa7c3622c935138
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-15 18:09:32 -0700

    Update deleteVendor.php

--- model/vendor/deleteVendor.php
+++ model/vendor/deleteVendor.php
@@ -1,41 +1,47 @@
 <?php
 	require_once('../../include/config/constants.php');
-	require_once('../../include/config/db.php');
+	require_once('../../include/config/dbconnect.php');
 	
 	if(isset($_POST['venID'])){
 		
-		$vendorID = htmlentities($_POST['venID']);
+		$venID = htmlentities($_POST['vendorID']);
 		
 		/*  Check if mandatory fields are not empty */
-		if(!empty($vendorID)){
+		if(!empty($venID)){
 			
 			/*  Sanitize vendorID */
-			$vendorID = filter_var($vendorID, FILTER_SANITIZE_STRING);
+			$venID = filter_var($venID, FILTER_SANITIZE_STRING);
 
 			/*  get VendorID is in the database */
-			$qVendor = 'SELECT vendorID FROM vendor WHERE vendorID=:vendorID';
-			$getVendorStatement = $conn->prepare($qVendor);
-			$getVendorStatement->execute(['vendorID' => $vendorID]);
+			$qVen = 'SELECT vendorID FROM vendor WHERE vendorID=:venID';
+			$venStatement = $dbcon->prepare($qVen);
+			$venStatement->execute(['vendorID' => $venID]);
 			
-			if($getVendorStatement->rowCount() > 0){
+			if($venStatement->rowCount() > 0){
 				
 				/*  Vendor exists in DB. start the DELETE process */
-				$delVendor = 'DELETE FROM vendor WHERE vendorID=:vendorID';
-				$delVendorStatement = $conn->prepare($delVendor);
-				$delVendorStatement->execute(['vendorID' => $vendorID]);
+				$delVen = 'DELETE FROM vendor WHERE vendorID=:venID';
+				$venStatement = $dbcon->prepare($delVen);
+				$venStatement->execute(['vendorID' => $venID]);
 
-				echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Vendor deleted.</div>';
+				echo '<div class="alert alert-success">
+                                           <button type="button" class="close" data-dismiss="alert">&times;</button>Vendor deleted.
+				      </div>';
 				exit();
 				
 			} else {
 				/*  Vendor does not exist, therefore, tell the user that he can't delete that vendor */ 
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Vendor does not exist in DB. Therefore, can\'t delete.</div>';
+				echo '<div class="alert alert-danger">
+                                           <button type="button" class="close" data-dismiss="alert">&times;</button>Vendor does not exist in DB. Therefore, can\'t delete.
+                                      </div>';
 				exit();
 			}
 			
 		} else {
 			/* vendorDI is empty. Therefore, display the error message */
-			echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter the Vendor ID</div>';
+			echo '<div class="alert alert-danger">
+                                   <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter the Vendor ID
+			      </div>';
 			exit();
 		}
 	}
