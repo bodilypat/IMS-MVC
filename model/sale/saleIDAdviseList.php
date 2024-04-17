cdc483623685a5b14cc631e7841002155093f877
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-17 15:17:03 -0700

    Add files via upload

---
+++ model/sale/saleIDAdviseList.php
@@ -0,0 +1,30 @@
+<?php
+	require_once('../../include/config/constants.php');
+	require_once('../../include/config/dbconnect.php');
+	
+	/* Check  POST  execute the script */
+	if(isset($_POST['textBoxValue'])){
+		$output = '';
+		$saleIDString = '%' . htmlentities($_POST['textBoxValue']) . '%';
+		
+		/* get the saleID */
+		$qSale = 'SELECT saleID FROM sale WHERE saleID LIKE ?';
+		$saleStatement = $conn->prepare($qSale);
+		$saleStatement->execute([$saleIDString]);
+		
+		/* // If we receive any results from the above query, then display them in a list*/
+ 	if($saleStatetment->rowCount() > 0){
+			
+			/*  sale ID is available in DB.create the dropdown list
+ */			$output = '<ul class="list-unstyled adviseList" id="saleIDAdviseList">';
+			while($result = $saleStatement->fetch(PDO::FETCH_ASSOC)){
+				$output .= '<li>' . $result['saleID'] . '</li>';
+			}
+			echo '</ul>';
+		} else {
+			$output = '';
+		}
+		$saleStatement->closeCursor();
+		echo $output;
+	}
+?>
