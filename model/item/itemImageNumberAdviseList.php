81fe258af0389d20f3faef27338605492d4a13af
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-17 15:41:44 -0700

    Add files via upload

---
+++ model/item/itemImageNumberAdviseList.php
@@ -0,0 +1,28 @@
+<?php
+	require_once('../../include/config/constants.php');
+	require_once('../../include/config/dbconnect.php');
+	
+	/*  Check POST request is received , execute the script */
+	if(isset($_POST['textBoxValue'])){
+		$output = '';
+		$itemNumString = '%' . htmlentities($_POST['textBoxValue']) . '%';
+		
+		/*  get the item name */
+		$qItem= 'SELECT itemNumber FROM item WHERE itemNumber LIKE ?';
+		$itemStatement = $dbcon->prepare($qInum);
+		$itemStatement->execute([$itemNumString]);
+		
+		/* display them in a list */
+		if($itemStatement->rowCount() > 0){
+			$output = '<ul class="list-unstyled AdviseList" id="itemImageNumberAdviseList">';
+			while($result = $itemStatememt->fetch(PDO::FETCH_ASSOC)){
+				$output .= '<li>' . $result['itemNumber'] . '</li>';
+			}
+			echo '</ul>';
+		} else {
+			$output = '';
+		}
+		$itemStatement->closeCursor();
+		echo $output;
+	}
+?>
