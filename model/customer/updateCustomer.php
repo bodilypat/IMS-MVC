33432051b3f7f20b3f0acc9dfc47bcf02f25f989
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-24 15:50:08 -0700

    Add files via upload

--- model/customer/updateCustomer.php
+++ model/customer/updateCustomer.php
@@ -1,116 +1,89 @@
 <?php
 	require_once('../../define/config/constants.php');
-	require_once('../../define/config/dbconnect.php');
+	require_once('../../define/config/db.php');
 	
-	/* Check if the POST query is received */
+	// Check if the POST query is received
 	if(isset($_POST['customerDetailsCustomerID'])) {
 		
-		$custID = htmlentities($_POST['custID']);
-		$custFullName = htmlentities($_POST['custFullName']);
-		$custMobile = htmlentities($_POST['custMobile']);
-		$custPhone = htmlentities($_POST['custPhone']);
-		$custEmail = htmlentities($_POST['custEmail']);
-		$custAddress = htmlentities($_POST['custAddress']);
-		$custAddress2 = htmlentities($_POST['custAddress2']);
-		$custCity = htmlentities($_POST['custCity']);
-		$custDistrict = htmlentities($_POST['custDistrict']);
-		$custStatus = htmlentities($_POST['custStatus']);
+		$customerID = htmlentities($_POST['customerID']);
+		$customerFullName = htmlentities($_POST['customerFullName']);
+		$customerMobile = htmlentities($_POST['customerMobile']);
+		$customerPhone = htmlentities($_POST['customerPhone']);
+		$customerEmail = htmlentities($_POST['customerEmail']);
+		$customerAddress = htmlentities($_POST['customerAddress']);
+		$customerAddress2 = htmlentities($_POST['customerAddress2']);
+		$customerCity = htmlentities($_POST['customerCity']);
+		$customerDistrict = htmlentities($_POST['customerDistrict']);
+		$customerStatus = htmlentities($_POST['customerStatus']);
 		
-		/*  Check if mandatory fields are not empty */
-
-		if(isset($custFullName) && isset($custMobile) && isset($custAddress)) {
+		// Check if mandatory fields are not empty
+		if(isset($customerFullName) && isset($customerMobile) && isset($customerAddress)) {
 			
-		
-			if(filter_var($custMobile, FILTER_VALIDATE_INT) === 0 || filter_var($custMobile, FILTER_VALIDATE_INT)) {
-				
+			// Validate mobile number
+			if(filter_var($customerMobile, FILTER_VALIDATE_INT) === 0 || filter_var($customerMobile, FILTER_VALIDATE_INT)) {
+				// Mobile number is valid
 			} else {
-				
-				echo '<div class="alert alert-danger">
-				           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid mobile number
-					  </div>';
+				// Mobile number is not valid
+				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid mobile number</div>';
 				exit();
 			}
 			
-			if(empty($custID)){
-				echo '<div class="alert alert-danger">
-				           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter the CustomerID to update that customer.
-				      </div>';
+			// Check if CustomerID field is empty. If so, display an error message
+			// We have to specifically tell this to user because the (*) mark is not added to that field
+			if(empty($custCustomerID)){
+				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter the CustomerID to update that customer.</div>';
 				exit();
 			}
 			
-			if(!empty($custPhone)){
-				if(filter_var($custPhone, FILTER_VALIDATE_INT) === 0 || filter_var($custPhone, FILTER_VALIDATE_INT)) {
-					
+			// Validate second phone number only if it's provided by user
+			if(!empty($customerPhone)){
+				if(filter_var($customerPhone, FILTER_VALIDATE_INT) === 0 || filter_var($customerPhone, FILTER_VALIDATE_INT)) {
+					// Phone number 2 is valid
 				} else {
-					
-					echo '<div class="alert alert-danger">
-					           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid number for phone number 2.
-						  </div>';
+					// Phone number 2 is not valid
+					echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid number for phone number 2.</div>';
 					exit();
 				}
 			}
 			
-			if(!empty($custEmail)) {
-				if (filter_var($custEmail, FILTER_VALIDATE_EMAIL) === false) {
-			
-					echo '<div class="alert alert-danger">
-					           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid email
-						  </div>';
+			// Validate email only if it's provided by user
+			if(!empty($customerEmail)) {
+				if (filter_var($customerEmail, FILTER_VALIDATE_EMAIL) === false) {
+					// Email is not valid
+					echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid email</div>';
 					exit();
 				}
 			}
 
+			// Check if the given CustomerID is in the DB
 			$qCust = 'SELECT customerID FROM customer WHERE customerID = :customerID';
-			$custStatement = $dbcon->prepare($qCust);
-			$custStatement->execute(['customerID' => $custID]);
+			$custStatement = $conn->prepare($custQuery);
+			$custStatement->execute(['customerID' => $customerID]);
 			
 			if($custStatement->rowCount() > 0) {
 				
-				/*  Construct the UPDATE query */
-				$editCust = 'UPDATE customer SET fullName = :fullName, 
-				                                 email = :email, 
-												 mobile = :mobile, 
-												 phone2 = :phone2, 
-												 address = :address, 
-												 address2 = :address2, 
-												 city = :city, 
-												 district = :district, 
-												 status = :status 
-											WHERE customerID = :custID';
-				$custStatement = $dbcon->prepare($editCust);
-				$custStatement->execute(['fullName' => $custFullName, 
-				                          'email' => $custEmail, 
-										  'mobile' => $custMobile, 
-										  'phone2' => $custPhone2, 
-										  'address' => $custAddress, 
-										  'address2' => $custAddress2, 
-										  'city' => $custCity, 
-										  'district' => $custDistrict, 
-										  'status' => $custStatus, 
-										  'customerID' => $custID]);
+				// CustomerID is available in DB. Therefore, we can go ahead and UPDATE its details
+				// Construct the UPDATE query
+				$editCust = 'UPDATE customer SET fullName = :customerFullName, email = :customerEmail, mobile = :CustomerMobile, phone = :customerPhone, address = :customerAddress, address2 = :customerAddress2, city = :customerCity, district = :customerDistrict, status = :customerStatus WHERE customerID = :customerID';
+				$updateCustStatement = $conn->prepare($editCust);
+				$updateCustStatement->execute(['fullName' => $customerFullName, 'email' => $customerEmail, 'mobile' => $customerMobile, 'phone2' => $customerPhone2, 'address' => $customerAddress, 'address2' => $customerAddress2, 'city' => $customerCity, 'district' => $customerDistrict, 'status' => $customerStatus, 'customerID' => $customerID]);
 				
-				/*  EDIT customer name in sale table too */
-				$editSale = 'UPDATE sale SET customerName = :custName WHERE customerID = :custID';
-				$saleStatement = $dbcon->prepare($editSale);
-				$saleStatement->execute(['customerName' => $custFullName, 'customerID' => $custID]);
+				// UPDATE customer name in sale table too
+				$editSale = 'UPDATE sale SET customerName = :customerName WHERE customerID = :customerID';
+				$updateSaleStatement = $conn->prepare($editSale);
+				$updateSaleStatement->execute(['customerName' => $customerFullName, 'customerID' => $customerID]);
 				
-				echo '<div class="alert alert-success">
-				           <button type="button" class="close" data-dismiss="alert">&times;</button>Customer details updated.
-					  </div>';
+				echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Customer details updated.</div>';
 				exit();
 			} else {
-				/* CustomerID is not in DB. Therefore, stop the update and quit */
-				echo '<div class="alert alert-danger">
-				           <button type="button" class="close" data-dismiss="alert">&times;</button>CustomerID does not exist in DB. Therefore, update not possible.
-					  </div>';
+				// CustomerID is not in DB. Therefore, stop the update and quit
+				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>CustomerID does not exist in DB. Therefore, update not possible.</div>';
 				exit();
 			}
 			
 		} else {
-			/* One or more mandatory fields are empty. Therefore, display the error message */
-			echo '<div class="alert alert-danger">
-			           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter all fields marked with a (*)
-				  </div>';
+			// One or more mandatory fields are empty. Therefore, display the error message
+			echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter all fields marked with a (*)</div>';
 			exit();
 		}
 	}
