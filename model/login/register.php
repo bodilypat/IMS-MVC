d84c498ba29a947be498918e8770fc5aebe0395f
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-24 08:50:51 -0700

    Add files via upload

--- model/login/register.php
+++ model/login/register.php
@@ -1,72 +1,75 @@
 <?php
-	require_once('../../inc/config/constants.php');
-	require_once('../../inc/config/db.php');
-	
-	$registerFullName = '';
-	$registerUsername = '';
-	$registerPassword1 = '';
-	$registerPassword2 = '';
-	$hashedPassword = '';
-	
-	if(isset($_POST['registerUsername'])){
-		$registerFullName = htmlentities($_POST['registerFullName']);
-		$registerUsername = htmlentities($_POST['registerUsername']);
-		$registerPassword1 = htmlentities($_POST['registerPassword1']);
-		$registerPassword2 = htmlentities($_POST['registerPassword2']);
-		
-		if(!empty($registerFullName) && !empty($registerUsername) && !empty($registerPassword1) && !empty($registerPassword2)){
-			
-			/*  Sanitize name */
-			$registerFullName = filter_var($registerFullName, FILTER_SANITIZE_STRING);
-			
-			/*  Check if name is empty */
-			if($registerFullName == ''){
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter your name.</div>';
-				exit();
-			}
-			
-			/* Check if username is empty */
-			if($registerUsername == ''){
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter your username.</div>';
-				exit();
-			}
-			
-			/* Check if both passwords are empty */
-			if($registerPassword1 == '' || $registerPassword2 == ''){
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter both passwords.</div>';
-				exit();
-			}
-			
-			/* Check if username is available */
-			$qUser = 'SELECT * FROM user WHERE username = :username';
-			$usernameCheckStatement = $conn->prepare($qUser);
-			$usernameCheckStatement->execute(['username' => $registerUsername]);
-			
-			if($usernameCheckStatement->rowCount() > 0){
-				/*  Username already exists. Hence can't create a new user */
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Username not available. Please select a different username.</div>';
-				exit();
-			} else {
-				/*  Check if passwords are equal */
-				if($registerPassword1 !== $registerPassword2){
-					echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Passwords do not match.</div>';
-					exit();
-				} else {
-					/* Start inserting user to DB */
-					/* Encrypt the password */
-					$hashedPassword = md5($registerPassword1);
-					$insertUserSql = 'INSERT INTO user(fullName, username, password) VALUES(:fullName, :username, :password)';
-					$insertUserStatement = $conn->prepare($insertUserSql);
-					$insertUserStatement->execute(['fullName' => $registerFullName, 'username' => $registerUsername, 'password' => $hashedPassword]);
-					
-					echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Registration complete.</div>';
-					exit();
-				}
-			}
-		} else {
-			/*  One or more mandatory fields are empty. display a the error message */
-			echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter all fields marked with a (*)</div>';
-			exit();
-		}
-	}
+     require_once('../assign/config/constants.php');
+     require_once('../assign/config/dbconnect.php');
+
+     $FullName = '';
+     $Username = '';
+     $Password = '';
+     $ConfirmPassword = '';
+     $hashedPass = '';
+
+     if(isset($_POST['username']))
+     {
+        $FullName = htmentities($_POST['fullName']);
+        $Username = htmlentities($_POST['username']);
+        $Password = htmlentities($_POST['password']);
+        $ConfirmPassword = htmlentities($_POST['confirmPassword']);
+
+        if(!empty($FullName) && !empty($Username) && !empty($Password) && !empty($ConfirmPassword))
+        {
+            $FullName == filter_var($FullName, FILTER_SANITIZE_STRING);
+            if($FullName == '')
+            {
+                echo '<div class="alert-danger">
+                            <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter your name.
+                      </div>';
+                      exit();
+            }
+            if($Username == '')
+            {
+                echo '<div class="alert alert-danger">
+                          <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter your username.
+                      </div>';
+                exit();
+            }
+            if($Password == '' || $ConfirmPassword == '')
+            {
+                echo '<div class="alert alert-danger">
+                          <button type="button" class="close" data-dimiss="alert">&times;<button>Please enter your username.</div>';
+                exit();
+            }
+            /* check if username is available  */
+            $qUser = 'SELECT * FROM user WHERE username = :Username';
+            $userStatement = $dbcon->prepare($qUser);
+            $userStatement->execute(['username' => $Username]);
+
+            if($userStatement->rowCount() > 0){
+                echo '<div class="alert alert-danger">
+                          <button type="button" class="close" data-dismiss="alear">&times;</button>/Username not available.select different username.
+                      </div>';
+                exit();
+            } else {
+                if($Password !== $ConfirmPassword){
+                    echo '<div class="alert alert-danger">
+                               <button typ=e"button" class="close" data-dismiss="alert">&times;</button>Password do not match.
+                          </div>';
+                    exit();
+                } else {
+                    $hashedPassword = md5($Password);
+                    $addUser = 'INSERT INTO user(fullname, username, password) VALUES(:FullName, :Username, :Password)';
+                    $userStatement = $dbcon->prepare($addUser);
+                    $userStatement->execute(['fullname'=> $FullName, 'username' => $Username, 'password' => $hashedPass]);
+                    echo '<div class="alert alert-success">
+                              <button type="button" class="close data-dismiss="alert">&times;<button>Registration complete.
+                          </div>';
+                    exit();
+                }
+            }
+        } else {
+            echo '<div class="close"alert alert-danger">
+                       <button type="button" class="close" data-dismiss="alert">&times;<button>Please enter field marked with a(*)
+                  </div>';
+            exit();
+        }
+     }
 ?>
