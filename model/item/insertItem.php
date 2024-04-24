f357c5c8bb0951a96c66f6598042c2179e5bc8d0
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-24 13:46:49 -0700

    Add files via upload

--- model/item/insertItem.php
+++ model/item/insertItem.php
@@ -1,52 +1,58 @@
 <?php
-	require_once('../../include/config/constants.php');
-	require_once('../../include/config/dbconnect.php');
+	require_once('../../define/config/constants.php');
+	require_once('../../define/config/dbconnect.php');
 	
 	$balanceStock = 0;
 	$baseImageFolder = '../../data/item_images/';
 	$itemImageFolder = '';
 	
-	if(isset($_POST['itemDetailsItemNumber']))
+	if(isset($_POST['itemNumber']))
 	{
 		
-		$itemNum = htmlentities($_POST['itemNumber']);
+		$itemNumber = htmlentities($_POST['itemNumber']);
 		$itemName = htmlentities($_POST['itemName']);
 		$itemDiscount = htmlentities($_POST['itemDiscount']);
 		$itemQuantity = htmlentities($_POST['itemQuantity']);
 		$itemUnitPrice = htmlentities($_POST['itemUnitPrice']);
 		$itemStatus = htmlentities($_POST['itemStatus']);
-		$itemDes = htmlentities($_POST['itemDescription']);
+		$itemDescription = htmlentities($_POST['itemDescription']);
 		
 		/* Check if mandatory fields are not empty */
-		if(!empty($itemNum) && !empty($itemName) && isset($itemquantity) && isset($itemUnitPrice)){
+		if(!empty($itemNumber) && !empty($itemName) && isset($itemQuantity) && isset($itemUnitPrice)){
 			
 			/*  Sanitize item number */
-			$itemNum = filter_var($itemNum, FILTER_SANITIZE_STRING);
+			$itemNumber = filter_var($itemNumber, FILTER_SANITIZE_STRING);
 			
 			/*  Validate item quantity. It has to be a number */
 			if(filter_var($itemQuantity, FILTER_VALIDATE_INT) === 0 || filter_var($itemQuantity, FILTER_VALIDATE_INT)){
 				
 			} else {
 				
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid number for quantity</div>';
+				echo '<div class="alert alert-danger">
+				          <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid number for quantity
+					  </div>';
 				exit();
 			}
 					
 			if(filter_var($itemUnitPrice, FILTER_VALIDATE_FLOAT) === 0.0 || filter_var($itemUnitPrice, FILTER_VALIDATE_FLOAT)){			
 			} else {
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid number for unit price</div>';
+				echo '<div class="alert alert-danger">
+				           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid number for unit price
+					  </div>';
 				exit();
 			}
 			
 			if(!empty($itemDiscount)){
 				if(filter_var($itemDiscount, FILTER_VALIDATE_FLOAT) === false){
-					echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid discount amount</div>';
+					echo '<div class="alert alert-danger">
+					          <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid discount amount
+						  </div>';
 					exit();
 				}
 			}
 			
 			/* Create image folder for uploading images */
-			$itemImageFolder = $baseImageFolder . $itemNum;
+			$itemImageFolder = $baseImageFolder . $itemNumber;
 			if(is_dir($itemImageFolder)){
 				/*  Folder already exist. do nothing */
 			} else {
@@ -54,31 +60,37 @@
 				mkdir($itemImageFolder);
 			}			
 			/*  Calculate the stock values */
-			$qITEMk = 'SELECT stock FROM item WHERE itemNumber=:itemNum';
+			$qItem = 'SELECT stock FROM item WHERE itemNumber=:itemNumber';
 			$itemStatement = $dbcon->prepare($qItem);
-			$itemStatement->execute(['itemNumber' => $itemNum]);
+			$itemStatement->execute(['itemNumber' => $itemNumber]);
 			if($itemStatement->rowCount() > 0){				
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Item already exists in DB. Please click the <strong>Update</strong> button to update the details. Or use a different Item Number.</div>';
+				echo '<div class="alert alert-danger">
+				           <button type="button" class="close" data-dismiss="alert">&times;</button>Item already exists in DB. Please click the <strong>Update</strong> button to update the details. Or use a different Item Number.
+					  </div>';
 				exit();
 			} else {
 								
 				$addItem = 'INSERT INTO item(itemNumber, itemName, discount, stock, unitPrice, status, description) 
-                                            VALUES(:itemNum, :itemName, :itemDiscount, :itemStock, :itemUnitPrice, :itemStatus, :itemDes)';
+				            VALUES(:itemNumber, :itemName, :itemDiscount, :itemStock, :itemUnitPrice, :itemStatus, :itemDescription)';
 				$itemStatement = $dbcon->prepare($addItem);
-				$itemStatement->execute(['itemNumber' => $itemNum, 
-							'itemName' => $itemName, 
-							'discount' => $itemDiscount, 
-							'stock' => $itemQuantity, 
-							'unitPrice' => $itemUnitPrice, 
-							'status' => $itemStatus, 
-							'description' => $itemDes]);
-				echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Item added to database.</div>';
+				$itemStatement->execute(['itemNumber' => $itemNumber, 
+				                         'itemName' => $itemName, 
+										 'discount' => $itemDiscount, 
+										 'stock' => $itemQuantity, 
+										 'unitPrice' => $itemUnitPrice, 
+										 'status' => $itemStatus, 
+										 'description' => $itemDescription]);
+				echo '<div class="alert alert-success">
+				            <button type="button" class="close" data-dismiss="alert">&times;</button>Item added to database.
+				      </div>';
 				exit();
 			}
 
 		} else {
 			/* One or more mandatory fields are empty. Therefore, display a the error message */
-			echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter all fields marked with a (*)</div>';
+			echo '<div class="alert alert-danger">
+			           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter all fields marked with a (*)
+				  </div>';
 			exit();
 		}
 	}
