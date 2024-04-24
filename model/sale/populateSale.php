8060caa4bf48532518ab892f72fc275460d1b0c4
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-24 15:27:37 -0700

    Update populateSale.php

--- model/sale/populateSale.php
+++ model/sale/populateSale.php
@@ -1,6 +1,6 @@
 <?php
-	require_once('../../include/config/constants.php');
-	require_once('../../include/config/dbconnect.php');
+	require_once('../../define/config/constants.php');
+	require_once('../../define/config/dbconnect.php');
 
 	// Execute the script if the POST request is submitted
 	if(isset($_POST['saleID'])){
@@ -8,13 +8,13 @@
 		$saleID = htmlentities($_POST['saleID']);
 		
 		$qSale = 'SELECT * FROM sale WHERE saleID = :saleID';
-		$saleStatement = $conn->prepare($qSale);
+		$saleStatement = $dbcon->prepare($qSale);
 		$saleStatement->execute(['saleID' => $saleID]);
 		
 		// If data is found for the given saleID, return it as a json object
 		if($saleStatement->rowCount() > 0) {
-			$recordSale = $saleStatement->fetch(PDO::FETCH_ASSOC);
-			echo json_encode($recordSale);
+			$result = $saleStatement->fetch(PDO::FETCH_ASSOC);
+			echo json_encode($result);
 		}
 		$saleStatement->closeCursor();
 	}
