a8658131d9c34b33ff27e444c124aba7428894b3
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-24 10:25:32 -0700

    Create footer.php

---
+++ define/footer.php
@@ -0,0 +1,9 @@
+    <!-- Footer -->
+    <footer class="footer bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Inventory System <?php echo date('Y'); ?></p>
+      </div>
+    </footer>
+<!-- Custom scripts -->
+	<script src="assign/js/scripts.js"></script>
+	<script src="assign/js/login.js"></script>
