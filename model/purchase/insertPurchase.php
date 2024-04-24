21a74468951586889af4c11e9813b2c929fb59a6
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-24 13:46:14 -0700

    Add files via upload

--- model/purchase/insertPurchase.php
+++ model/purchase/insertPurchase.php
@@ -2,38 +2,47 @@
 	require_once('../../define/config/constants.php');
 	require_once('../../define/config/dbconnect.php');
 	
-	if(isset($_POST['purchaseItemNumber'])){
-		$purchItemNumber = htmlentities($_POST['purchaseItemNumber']);
-		$purchDate = htmlentities($_POST['purchaseDate']);
-		$purchaItemName = htmlentities($_POST['purchaseItemName']);
-		$purchQuantity = htmlentities($_POST['purchaseQuantity']);
-		$purchUnitPrice = htmlentities($_POST['purchaseUnitPrice']);
-		$purchVendorName = htmlentities($_POST['purchaseVendorName']);
+	if(isset($_POST['purchItemNumber'])){
+
+		$purchItemNumber = htmlentities($_POST['purchItemNumber']);
+		$purchDate = htmlentities($_POST['purchDate']);
+		$purchItemName = htmlentities($_POST['purchItemName']);
+		$purchQuantity = htmlentities($_POST['purchQuantity']);
+		$purchUnitPrice = htmlentities($_POST['purchUnitPrice']);
+		$purchVendorName = htmlentities($_POST['purchVendorName']);
 		
 		$balanceStock = 0;
 		$newStock = 0;
 		
 		/* Check if mandatory fields are not empty */
-		if(isset($purchItemNumber) && isset($purchDate) && isset($purchaItemName) && isset($purchQuantity) && isset($purchUnitPrice)){
+		if(isset($purchItemNumber) && isset($purchDate) && isset($purchItemName) && isset($purchQuantity) && isset($purchUnitPrice)){
 			
 			if($purchItemNumber == ''){ 
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter Item Number.</div>';
+				echo '<div class="alert alert-danger">
+				           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter Item Number.
+					  </div>';
 				exit();
 			}
 						
 			if($purchItemName == ''){ 
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter Item Name.</div>';
+				echo '<div class="alert alert-danger">
+				           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter Item Name.
+					  </div>';
 				exit();
 			}
 			
 			if($purchQuantity == ''){ 
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter Quantity.</div>';
+				echo '<div class="alert alert-danger">
+				           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter Quantity.
+					  </div>';
 				exit();
 			}
 			
 			
 			if($purchUnitPrice == ''){ 
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter Unit Price.</div>';
+				echo '<div class="alert alert-danger">
+				           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter Unit Price.
+					  </div>';
 				exit();
 			}
 			
@@ -42,46 +51,49 @@
 			if(filter_var($purchQuantity, FILTER_VALIDATE_INT) === 0 || filter_var($purchQuantity, FILTER_VALIDATE_INT)){
 			} else {
 				/* Quantity is not a valid number */
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid number for quantity.</div>';
+				echo '<div class="alert alert-danger">
+				           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid number for quantity.
+					  </div>';
 				exit();
 			}
 			
 			/* Validate unit price. It has to be an integer or floating point value */
-		       if(filter_var($purchUnitPrice, FILTER_VALIDATE_FLOAT) === 0.0 || filter_var($purchUnitPrice, FILTER_VALIDATE_FLOAT)){
+		    if(filter_var($purchUnitPrice, FILTER_VALIDATE_FLOAT) === 0.0 || filter_var($purchUnitPrice, FILTER_VALIDATE_FLOAT)){
 				
 			} else {
 				
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid number for unit price.</div>';
+				echo '<div class="alert alert-danger">
+				           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid number for unit price.
+					  </div>';
 				exit();
 			}
 						 
 			/* calculate the stock values and update to match the new purchase quantity */
 			$qItem= 'SELECT stock FROM item WHERE itemNumber=:purchItemNumber';
-			$itemStatement = $conn->prepare($qItem);
+			$itemStatement = $dbcon->prepare($qItem);
 			$itemStatement->execute(['itemNumber' => $purchItemNumber]);
 
 			if($itemStatement->rowCount() > 0){
 				
 				/* Get vendorId for the given vendorName */
 				
-				$qVen = 'SELECT * FROM vendor WHERE fullName = :purchVendorName';
-				$venStatement = $conn->prepare($qVen);
+				$qVen = 'SELECT * FROM vendor WHERE fullName = :$purchVendorName';
+				$venStatement = $dbcon->prepare($qVen);
 				$venStatement->execute(['fullName' => $purchVendorName]);
 
 				$resultVen = $venStatement->fetch(PDO::FETCH_ASSOC);
-				$venID = $resultVen['vendorID'];
+				$vendorID = $resultVen['vendorID'];
 				
 				/*  Item exits in the item table, inserting data to purchase table */
-				$addPurch = 'INSERT INTO purchase(itemNumber, purchaseDate, itemName, unitPrice, quantity, vendorName, vendorID) 
-                                             VALUES(:purchItemNumber, :purchDate, :purchsItemName, :purchUnitPrice, :purchQuantity, :purchVendorName, :purchVendorID)';
-				$purchStatement = $conn->prepare($addPurch);
-				$purchStatement->execute(['itemNumber' => $purchItemNumber, 
-							  'purchaseDate' => $purchDate, 
-							  'itemName' => $purchItemName, 
-							  'unitPrice' => $purchUnitPrice, 
-							  'quantity' => $purchQuantity,  
-							  'vendorName' => $purchVendorName, 
-							  'vendorID' => $venID]);
+				$addPur = 'INSERT INTO purchase(itemNumber, purchaseDate, itemName, unitPrice, quantity, vendorName, vendorID) VALUES(:itemNumber, :purchaseDate, :itemName, :unitPrice, :quantity, :vendorName, :vendorID)';
+				$PurStatement = $dbcon->prepare($addPur);
+				$PurStatement->execute(['itemNumber' => $purchItemNumber, 
+				                        'purchaseDate' => $purchDate, 
+										'itemName' => $purchItemName, 
+										'unitPrice' => $purchUnitPrice, 
+										'quantity' => $purchQuantity, 
+										'vendorName' => $purchVendorName, 
+										'vendorID' => $vendorID]);
 				
 				/*  Calculate the new stock value using the existing stock in item table */
 				$resultItem = $itemStatement->fetch(PDO::FETCH_ASSOC);
@@ -89,23 +101,29 @@
 				$newStock = $balanceStock + $purchQuantity;
 				
 				/* Edit the new stock value in item table */
-				$editItem = 'UPDATE item SET stock = :stock WHERE itemNumber = :purchIitemNumber';
+				$editItem = 'UPDATE item SET stock = :stock WHERE itemNumber = :purchItemNumber';
 				$itemStatement = $dbcon->prepare($editItem);
-				$itemStatement->execute(['stock' => $newStock, 'itemNumber' => $purchaseItemNumber]);
+				$itemStatement->execute(['stock' => $newStock, 'itemNumber' => $purchItemNumber]);
 				
-				echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Purchase details added to database and stock values updated.</div>';
+				echo '<div class="alert alert-success">
+				           <button type="button" class="close" data-dismiss="alert">&times;</button>Purchase details added to database and stock values updated.
+					  </div>';
 				exit();
 				
 			} else {
 				/* Item does not exist in item table */ 
 				/*  to add it to DB as a new purchase */
-				echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Item does not exist in DB. Therefore, first enter this item to DB using the <strong>Item</strong> tab.</div>';
+				echo '<div class="alert alert-danger">
+				           <button type="button" class="close" data-dismiss="alert">&times;</button>Item does not exist in DB. Therefore, first enter this item to DB using the <strong>Item</strong> tab.
+					  </div>';
 				exit();
 			}
 
 		} else {
 			/* One or more mandatory fields are empty. display a the error message */
-			echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Please enter all fields marked with a (*)</div>';
+			echo '<div class="alert alert-danger">
+			           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter all fields marked with a (*)
+				  </div>';
 			exit();
 		}
 	}
