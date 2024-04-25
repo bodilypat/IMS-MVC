481632116206a7ffa16490d874e7757b394daba8
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-25 09:02:10 -0700

    Add files via upload

--- model/item/getItemStock.php
+++ model/item/getItemStock.php
@@ -7,11 +7,12 @@
 		
 		$itemNumber = htmlentities($_POST['itemNumber']);
 		
-		$qItem = 'SELECT * FROM item WHERE itemNumber = :itemNumber';
+		$qItem = "SELECT * FROM item WHERE itemNumber = '$itemNumber'";
 		$itemStatement = $dbcon->prepare($qItem);
 		$itemStatement->execute(['itemNumber' => $itemNum]);
 		
 		/* get object item from database , return it as a json object */
+		
 		if($itemStatement->rowCount() > 0) {
 			$resultset = $itemStatement->fetch(PDO::FETCH_ASSOC);
 			echo json_encode($resultset);
