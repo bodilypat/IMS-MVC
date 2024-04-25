de2c887c07993faf6d73d55f3d371b3a6bebef00
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-25 10:06:18 -0700

    Add files via upload

--- model/item/getItemName.php
+++ model/item/getItemName.php
@@ -1,20 +1,20 @@
 <?php
 	require_once('../../define/config/constants.php');
-	require_once('../../define/config/dbconnect.php');
+	require_once('../../define/config/db.php');
 
-	/* POST request is submitted */
+	// Execute the script if the POST request is submitted
 	if(isset($_POST['itemNumber'])){
 		
 		$itemNumber = htmlentities($_POST['itemNumber']);
 		
-		$qItem = 'SELECT * FROM item WHERE itemNumber = :itemNumber';
-		$itemStatement = $dbcon->prepare($qItem);
+		$queryItem = "SELECT * FROM item WHERE itemNumber = '$itemNumber'";
+		$itemStatement = $dbcon->prepare($queryItem);
 		$itemStatement->execute(['itemNumber' => $itemNumber]);
 		
-		/*  If data is found for the given item number, return it as a json object */
+		// get item object , return it as a json object
 		if($itemStatement->rowCount() > 0) {
-			$result = $itemStatement->fetch(PDO::FETCH_ASSOC);
-			echo json_encode($result);
+			$resultset = $itemStatement->fetch(PDO::FETCH_ASSOC);
+			echo json_encode($resultset);
 		}
 		$itemStatement->closeCursor();
 	}
