94fe659399620fab5cccb400583babc214c1952b
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-25 08:14:21 -0700

    Add files via upload

--- model/vendor/populateVendor.php
+++ model/vendor/populateVendor.php
@@ -2,7 +2,7 @@
 	require_once('../../define/config/constants.php');
 	require_once('../../define/config/db.php');
 
-	// Execute the script if the POST request is submitted
+	/* Execute the script if the POST request is submitted */
 	if(isset($_POST['vendorID'])){
 		
 		$vendorID = htmlentities($_POST['vendorID']);
@@ -11,7 +11,7 @@
 		$venStatement = $dbcon->prepare($qVen);
 		$venStatement->execute(['vendorID' => $vendorID]);
 		
-		// If data is found for the given vendorID, return it as a json object
+		/* get record form vendor table */
 		if($venStatement->rowCount() > 0) {
 			$resultset = $venStatement->fetch(PDO::FETCH_ASSOC);
 			echo json_encode($resultset);
