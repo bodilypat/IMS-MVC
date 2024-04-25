b9ec41864f1539a609634b6bf0a2989eab15b888
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-25 06:02:16 -0700

    Add files via upload

--- model/item/getItemStock.php
+++ model/item/getItemStock.php
@@ -1,20 +1,20 @@
 <?php
-	require_once('../../include/config/constants.php');
-	require_once('../../include/config/dbconnect.php');
+	require_once('../../define/config/constants.php');
+	require_once('../../define/config/dbconnect.php');
 
 	/* Execute the POST request is submitted */
 	if(isset($_POST['itemNumber'])){
 		
-		$itemNum = htmlentities($_POST['itemNumber']);
+		$itemNumber = htmlentities($_POST['itemNumber']);
 		
-		$qItem = 'SELECT * FROM item WHERE itemNumber = :itemNum';
+		$qItem = 'SELECT * FROM item WHERE itemNumber = :itemNumber';
 		$itemStatement = $dbcon->prepare($qItem);
 		$itemStatement->execute(['itemNumber' => $itemNum]);
 		
 		/* If data is found for the given item number, return it as a json object */
 		if($itemStatement->rowCount() > 0) {
-			$resultset = $itemStatement->fetch(PDO::FETCH_ASSOC);
-			echo json_encode($resultset);
+			$result = $itemStatement->fetch(PDO::FETCH_ASSOC);
+			echo json_encode($result);
 		}
 		$itemStatement->closeCursor();
 	}
