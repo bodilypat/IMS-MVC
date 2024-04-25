df796634a12c2e7b1a88e39f60c08fb4d50403a1
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-25 12:53:07 -0700

    Add files via upload

--- model/customer/customerSearchTableCreator.php
+++ model/customer/customerSearchTableCreator.php
@@ -1,6 +1,6 @@
 <?php
-	require_once('../../define/config/constants.php');
-	require_once('../../define/config/dbconnect.php');
+	require_once('../../include/config/constants.php');
+	require_once('../../include/config/dbconnect.php');
 	
 	$qCust = 'SELECT * FROM customer';
 	$custStatement = $dbcon->prepare($qCust);
@@ -13,7 +13,7 @@
 						<th>Full Name</th>
 						<th>Email</th>
 						<th>Mobile</th>
-						<th>Phone 2</th>
+						<th>Phone </th>
 						<th>Address</th>
 						<th>Address 2</th>
 						<th>City</th>
@@ -23,14 +23,14 @@
 				</thead>
 				<tbody>';
 	
-	/*  Create table rows from the selected data */
+	/*  Create customer table from object selected in DB */
 	while($resultset = $custStatement->fetch(PDO::FETCH_ASSOC)){
 		$output .= '<tr>' .
 						'<td>' . $resultset['customerID'] . '</td>' .
 						'<td>' . $resultset['fullName'] . '</td>' .
 						'<td>' . $resultset['email'] . '</td>' .
 						'<td>' . $resultset['mobile'] . '</td>' .
-						'<td>' . $resultset['phone2'] . '</td>' .
+						'<td>' . $resultset['phone'] . '</td>' .
 						'<td>' . $resultset['address'] . '</td>' .
 						'<td>' . $resultset['address2'] . '</td>' .
 						'<td>' . $resultset['city'] . '</td>' .
@@ -48,7 +48,7 @@
 							<th>Full Name</th>
 							<th>Email</th>
 							<th>Mobile</th>
-							<th>Phone 2</th>
+							<th>Phone </th>
 							<th>Address</th>
 							<th>Address 2</th>
 							<th>City</th>
