82a00900b9d0d3db7b1eb1ef921094f4b9e8b3b7
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-25 11:51:09 -0700

    Add files via upload

--- model/purchase/populatePurchase.php
+++ model/purchase/populatePurchase.php
@@ -2,19 +2,18 @@
 	require_once('../../define/config/constants.php');
 	require_once('../../define/config/dbconnect.php');
 
-/*  Execute the script if the POST request is submitted */
 	if(isset($_POST['purchaseID'])){
 		
 		$purchaseID = htmlentities($_POST['purchaseID']);
 		
-		$qPurch = 'SELECT * FROM purchase WHERE purchaseID = :purchaseID';
-		$purchStatement = $dbcon->prepare($qPurch);
+		$qPurch = " SELECT * FROM purchase WHERE purchaseID = '$purchaseID'";
+		$purchStatement = $conn->prepare($qPurch);
 		$purchStatement->execute(['purchaseID' => $purchaseID]);
 		
-		/* If data is found for the given purchaseID, return it as a json object */
+		/* get purchase object from database, return by json object */
 		if($purchStatement->rowCount() > 0) {
-			$result = $purchStatement->fetch(PDO::FETCH_ASSOC);
-			echo json_encode($result);
+			$resultset = $purchStatement->fetch(PDO::FETCH_ASSOC);
+			echo json_encode($resultset);
 		}
 		$purchStatement->closeCursor();
 	}
