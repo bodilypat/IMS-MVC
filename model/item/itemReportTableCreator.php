627d4d282cd112642f8a0a814e60f19af54f3947
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-08-14 09:24:13 -0700

    Add files via upload

--- model/item/itemReportTableCreator.php
+++ model/item/itemReportTableCreator.php
@@ -1,55 +1,56 @@
 <?php
-	require_once('../../include/config/constants.php');
-	require_once('../../include/config/dbconnect.php');
-	
-	$qItem = 'SELECT * FROM item';
-	$itemStatement = $dbconn->prepare($qItem);
-	$itemStatement->execute();
-	/* get item object from database */
-	$output = '<table id="itemReportTable" class="table table-sm table-striped table-bordered table-hover" style="width:100%">
-				<thead>
-					<tr>
-						<th>Product ID</th>
-						<th>Item Number</th>
-						<th>Item Name</th>
-						<th>Discount %</th>
-						<th>Stock</th>
-						<th>Unit Price</th>
-						<th>Status</th>
-						<th>Description</th>
-					</tr>
-				</thead>
-				<tbody>';
-	
-	/* create item table from object in database */
-	while($resultset = $itemStatement->fetch(PDO::FETCH_ASSOC)){
-		$output .= '<tr>' .
-						'<td>' . $resultset['productID'] . '</td>' .
-						'<td>' . $resultset['itemNumber'] . '</td>' .						
-						'<td><a href="#" class="itemDetailsHover" data-toggle="popover" id="' . $resultset['productID'] . '">' . $resultset['itemName'] . '</a></td>' .
-						'<td>' . $resultset['discount'] . '</td>' .
-						'<td>' . $resultset['stock'] . '</td>' .
-						'<td>' . $resultset['unitPrice'] . '</td>' .
-						'<td>' . $resultset['status'] . '</td>' .
-						'<td>' . $resultset['description'] . '</td>' .
-					'</tr>';
-	}
-	
-	$itemStatement->closeCursor();
-	
-	$output .= '</tbody>
-					<tfoot>
-						<tr>
-							<th>Product ID</th>
-							<th>Item Number</th>
-							<th>Item Name</th>
-							<th>Discount %</th>
-							<th>Stock</th>
-							<th>Unit Price</th>
-							<th>Status</th>
-							<th>Description</th>
-						</tr>
-					</tfoot>
-				</table>';
-	echo $output;
+    require_once('../../define/config/constants.php');
+    require_once('../../define/config/dbConnect.php');
+
+    $qItem = "SELECT * FROM item ";
+    $itemStatement = $deal->prepare($qItem);
+    $itemStatement->execute();
+
+    $output = '<table id="itemReportTable" class="table table-sm table-striped table-bordered table-hover" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>Product ID</th>
+                            <th>Item Number</th>
+                            <th>Item Name</th>
+                            <th>Discount</th>
+                            <th>Stock</th>
+                            <th>Unit Price</th>
+                            <th>Status</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                <tbody>';
+    
+    /* create report table */
+
+    while($result = $itemStatement->fetch(PDO::FETCH_ASSOC)) {
+        $output .='<tr>' . 
+                        '<td>' . $result['productID'] . '</td>' . 
+                        '<td>' . $result['itemNumber'] . '</td>' .  
+                        '<td><a href="#" class="itemHover" data-toggle="popover" id="' . $result['productID'] . '">' . $result['itemName'] . '</a></td>' . 
+                        '<td>' . $result['discount'] . '</td>' . 
+                        '<td>' . $result['stock'] . '</td>' . 
+                        '<td>' . $result['unitPrice'] . '</td>' . 
+                        '<td>' . $result['status'] . '</td>' . 
+                        '<td>' . $result['descriipt'] . '</td>' . 
+                  '</tr>';
+    }
+
+    $itemStatement->closeCursor();
+
+    $output .= '</tbody>
+                    <tfoot>
+                        <tr>
+                            <th>Product ID</th>
+                            <th>Item Number</th>
+                            <th>Item Name</th>
+                            <th>Discount</th>
+                            <th>Stock</th>
+                            <th>Unit Price</th>
+                            <th>Status</th>
+                            <th>Description</th>
+                        </tr>
+                    </tfoot>
+                </table>';
+    echo $output;
 ?>
