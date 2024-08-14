901c0eb49fbe4eff681c32433d7c397b5f157c26
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-08-14 09:16:34 -0700

    Add files via upload

--- model/customer/populateLastCustomer.php
+++ model/customer/populateLastCustomer.php
@@ -1,12 +1,12 @@
 <?php
-	require_once('../../define/config/constants.php');
-	require_once('../../define/config/dbconnect.php');
-	
-	$qCust = "SELECT MAX(customerID) FROM customer";
-	$custStatementt = $dbcon->prepare($qCust);
-	$custStatement->execute();
-	$result = $custStatement->fetch(PDO::FETCH_ASSOC);
-	/* get result customerID */
-	echo $result['MAX(customerID)'];
-	$custStatement->closeCursor();
+    require_once('../../define/config/constant.php');
+    require_once('../../define/config/dbConnect.php');
+
+    $qCust = "SELECT MAX(customerID) FROM customer";
+    $custStatement = $deal->prepare($qCust);
+    $custStatement->execute();
+
+    $result = $custStatement->fetch(PDO::FETCH_ASSOC);
+    echo $result['MAX(customerID)'];
+    $custStatement->closeCursor();
 ?>
