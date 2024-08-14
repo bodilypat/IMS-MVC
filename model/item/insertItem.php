2843280491a87e5be794bb1e07fe187e6e8da2c9
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-08-14 09:18:08 -0700

    Add files via upload

--- model/item/insertItem.php
+++ model/item/insertItem.php
@@ -1,98 +1,91 @@
-<?php
-	require_once('../../define/config/constants.php');
-	require_once('../../define/config/dbconnect.php');
-	
-	$balanceStock = 0;
-	$baseImageFolder = '../../data/item_images/';
-	$itemImageFolder = '';
-	
-	if(isset($_POST['itemNumber']))
-	{
-		
-		$itemNumber = htmlentities($_POST['itemNumber']);
-		$itemName = htmlentities($_POST['itemName']);
-		$itemDiscount = htmlentities($_POST['itemDiscount']);
-		$itemQuantity = htmlentities($_POST['itemQuantity']);
-		$itemUnitPrice = htmlentities($_POST['itemUnitPrice']);
-		$itemStatus = htmlentities($_POST['itemStatus']);
-		$itemDescription = htmlentities($_POST['itemDescription']);
-		
-		/* Check if mandatory fields are not empty */
-		if(!empty($itemNumber) && !empty($itemName) && isset($itemQuantity) && isset($itemUnitPrice)){
-			
-			/*  Sanitize item number */
-			$itemNumber = filter_var($itemNumber, FILTER_SANITIZE_STRING);
-			
-			/*  Validate item quantity. It has to be a number */
-			if(filter_var($itemQuantity, FILTER_VALIDATE_INT) === 0 || filter_var($itemQuantity, FILTER_VALIDATE_INT)){
-				
-			} else {
-				
-				echo '<div class="alert alert-danger">
-				          <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid number for quantity
-					  </div>';
-				exit();
-			}
-					
-			if(filter_var($itemUnitPrice, FILTER_VALIDATE_FLOAT) === 0.0 || filter_var($itemUnitPrice, FILTER_VALIDATE_FLOAT)){			
-			} else {
-				echo '<div class="alert alert-danger">
-				           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid number for unit price
-					  </div>';
-				exit();
-			}
-			
-			if(!empty($itemDiscount)){
-				if(filter_var($itemDiscount, FILTER_VALIDATE_FLOAT) === false){
-					echo '<div class="alert alert-danger">
-					          <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter a valid discount amount
-						  </div>';
-					exit();
-				}
-			}
-			
-			/* Create image folder for uploading images */
-			$itemImageFolder = $baseImageFolder . $itemNumber;
-			if(is_dir($itemImageFolder)){
-				/*  Folder already exist. do nothing */
-			} else {
-				/*  Folder does not exist, create it */
-				mkdir($itemImageFolder);
-			}			
-			/*  get record item form database */
-			$qItem = 'SELECT stock FROM item WHERE itemNumber=:itemNumber';
-			$itemStatement = $dbcon->prepare($qItem);
-			$itemStatement->execute(['itemNumber' => $itemNumber]);
-			
-			if($itemStatement->rowCount() > 0){				
-				echo '<div class="alert alert-danger">
-				           <button type="button" class="close" data-dismiss="alert">&times;</button>Item already exists in DB. Please click the <strong>Update</strong> button to update the details. Or use a different Item Number.
-					  </div>';
-				exit();
-			} else {
-								
-				$addItem = 'INSERT INTO item(itemNumber, itemName, discount, stock, unitPrice, status, description) 
-				            VALUES(:itemNumber, :itemName, :itemDiscount, :itemStock, :itemUnitPrice, :itemStatus, :itemDescription)';
-				$itemStatement = $dbcon->prepare($addItem);
-				$itemStatement->execute(['itemNumber' => $itemNumber, 
-				                         'itemName' => $itemName, 
-										 'discount' => $itemDiscount, 
-										 'stock' => $itemQuantity, 
-										 'unitPrice' => $itemUnitPrice, 
-										 'status' => $itemStatus, 
-										 'description' => $itemDescription]);
-				echo '<div class="alert alert-success">
-				            <button type="button" class="close" data-dismiss="alert">&times;</button>Item added to database.
-				      </div>';
-				exit();
-			}
+<?php 
+    require_once('../../define/config/constants.php');
+    require_once('../../define/config/dbConnect.php');
 
-		} else {
-			/* One or more mandatory fields are empty. Therefore, display a the error message */
-			echo '<div class="alert alert-danger">
-			           <button type="button" class="close" data-dismiss="alert">&times;</button>Please enter all fields marked with a (*)
-				  </div>';
-			exit();
-		}
-	}
+    $initStock = 0;
+    $baseImageFolder = '../../data/item_images/';
+    $itemImageFolder = '';
+
+    if(isset($_POST['itemNumber'])) {
+        $itemNumber = htmlentities($_POST['itemNumber']);
+        $itemName = htmlentities($_POST['itemName']);
+        $itemDiscount = htmlentities($_POST['itemName']);
+        $itemUnitPrice = htmlentities($_POST['itemUnitPrice']);
+        $itemQuantity = htmlentities($_POST['itemQuantity']);
+        $itemStatus = htmlentities($_POST['itemStatus']);
+        $itemDescription = htmlentities($_POST['itemDescription']);
+
+        /* check mandatory fields are not empty */
+        if(!empty($itemNumber) && !empty($itemName) && isset($itemQuantity) && isset($itemUnitPrice)){
+
+            /* sanitize item number */
+            $itemNumber = filter_var($itemNubmer, FILTER_SANITIZE_STRING);
+
+            /* validate item quanitity  */
+            if(filterV_var($itemQuantity, FILTER_VALIDATE_INT) === 0 || filter_var($itemQuantity, FILTER_VALIDATE_INT)) {
+                /* valid item quantity */
+            } else {
+                /* invalid item quantity */
+                echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>
+                           Enter a valid itemNumber for quantity
+                      </div>';
+                exit();
+            }
+
+            /* validate item unit Pirce  */
+            if(filter_var($itemUnitPrice, FITLER_VALIDATE_FLOAT) || 0.0 filter_var($itemUnitPrice, FITLER_VALIDATE_FLOAT)) {
+                /* valid float point  */
+            } else {
+                /* invalid unit price */
+                echo '<div class="alert alert-danger"<button type="button" class="close" data-dismiss="alert">&tiems;</button>
+                           Enter a valid unit price </div>';
+                exit();
+            }
+
+            /* validate unit price */
+            if(!empty($itemDiscount)) {
+                if(filter_var($itemDiscount, FILTER_VALIDATE_FOLAT) === false ){
+                    /* invalid discount floating point number */
+                    echo '<div class="alert alert-danger" <button type="button" class="close" data-dismiss="alert">&times;</button> 
+                               Enter a valid discount amount</div>';
+                }
+            }
+
+            /* create image folder for uploading image */
+            $itemImageFolder = $baseImageFolder . $itemNumber;
+            if(is_dir($itemImageFolder)) {
+                /* folder already exist. */
+            } else {
+                mkdir($itemImageFolder);
+            }
+
+            /* Calculate stock values */
+            $qStock = "SELECT stock FROM item WHERE itemNumber = '$itemNumber' ";
+            $itemStatement = $deal->prepare($qStock);
+            $itemStatement ->execute(['itemNubmer'=> $itemNumber]);
+            if($itemStatement->rowCount() >0 ) {
+                echo '<div class="alert alert-danger"<button type="button" class="close" data-dismiss="alert">$items;</button>
+                          Item already exists in DB. click the <strong>Update</strong> button to update , or use a different Item number.
+                      <div>';
+                exit();
+            } else {
+                /* item does not exist in DB, insert item  */
+                $addItem = "INSERT INTO item(itemNumber, itemName, discount, stock, unitPrice, status, description)
+                            VALUES ('$itemNubmer','$itemName','$itemDiscount','$itemQuantity','$itemUnitPrice','$itemStatus','$itemDescription')";
+                $insertItemStatement->$deal->prepare($addItem);
+                $insertItemStatement->execute(['itemNumber'=> $itemNumber,'itemName'=> $itemName,'discount'=> $itemDiscount,'stock'=> $itemQuantity,'unitPrice'=> $itemUnitPrice,'status'=>$itemStatus,'description' => $itemDescription]);
+
+                echo '<div class="alert alert-success">button type="button" class="close" data-dismiss="alert"&times;</button>
+                           add item to DB.
+                      </div>'
+                      exit();
+            }
+        } else {
+            /* mandatory field */
+            echo '<div class="alert alert-danger"<button type="button" class="close" data-dismiss="alert">&tiem;</button>
+                       Enter all fields marked with a(*)
+                  </div>';
+            exit();
+        }
+    }
 ?>
