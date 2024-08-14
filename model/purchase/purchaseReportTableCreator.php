c69323f7d9fd537d1c8e0ff4035ffb364fa50a52
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-08-14 10:31:36 -0700

    Add files via upload

--- model/purchase/purchaseReportTableCreator.php
+++ model/purchase/purchaseReportTableCreator.php
@@ -1,71 +1,52 @@
 <?php
-	require_once('../../define/config/constants.php');
-	require_once('../../define/config/dbconnect.php');
-	
-	$unitPrice = 0;
-	$quantity = 0;
-	$totalPrice = 0;
-	
-	if(isset($_POST['startDate'])){
-		$startDate = htmlentities($_POST['startDate']);
-		$endDate = htmlentities($_POST['endDate']);
-		
-		$qPurch = " SELECT * FROM purchase WHERE purchaseDate BETWEEN '$startDate' AND '$endDate'";
-		$purchStatement = $dbcon->prepare($pPurch);
-		$purchStatement->execute(['startDate' => $startDate, 'endDate' => $endDate]);
+    require_once('../../define/config/constants.php');
+    require_once('../../define/config/dbConnect.php');
 
-		$output = '<table id="purchaseReportsTable" class="table table-sm table-striped table-bordered table-hover" style="width:100%">
-					<thead>
-						<tr>
-							<th>Purchase ID</th>
-							<th>Item Number</th>
-							<th>Purchase Date</th>
-							<th>Item Name</th>
-							<th>Vendor Name</th>
-							<th>Vendor ID</th>
-							<th>Quantity</th>
-							<th>Unit Price</th>
-							<th>Total Price</th>
-						</tr>
-					</thead>
-					<tbody>';
-		
-		/*  Create purchase table from  object in database */
-		while($resultset = $purchStatement->fetch(PDO::FETCH_ASSOC)){
-			  $unitPrice = $resultset['unitPrice'];
-		      $quantity = $resultset['quantity'];
-			  $totalPrice = $unitPrice * $quantity;
-		
-			$output .= '<tr>' .
-							'<td>' . $resultset['purchaseID'] . '</td>' .
-							'<td>' . $resultset['itemNumber'] . '</td>' .
-							'<td>' . $resultset['purchaseDate'] . '</td>' .
-							'<td>' . $resultset['itemName'] . '</td>' .
-							'<td>' . $resultset['vendorName'] . '</td>' .
-							'<td>' . $resultset['vendorID'] . '</td>' .
-							'<td>' . $resultset['quantity'] . '</td>' .
-							'<td>' . $resultset['unitPrice'] . '</td>' .
-							'<td>' . $totalPrice . '</td>' .
-						'</tr>';
-		}
-		
-		$purchStatement->closeCursor();
-		
-		$output .= '</tbody>
-						<tfoot>
-							<tr>
-								<th>Total</th>
-								<th></th>
-								<th></th>
-								<th></th>
-								<th></th>
-								<th></th>
-								<th></th>
-								<th></th>
-								<th></th>
-							</tr>
-						</tfoot>
-					</table>';
-		echo $output;
-	}
+    $unitPrice = 0;
+    $quantity = 0;
+    $totalPrice = 0;
+
+    $qPurch = "SELECT * from purchase" ;
+    $purchStatement = $deal->prepare($qPurcha);
+    $purchStatement->execute();
+
+    $output = '<table id="purchaseReportTable" class="table table-sm table-striped table-bordered table-hover" style="width:100%">
+                    <thead></thead>
+                    <tbody>';
+
+    /* create table rows from select data */
+    while($result = $purchStatement->fetch(PDO::FETCH_ASSOC)) {
+        $unitPrice = $result['unitPrice'];
+        $quantity = $result['quantity'];
+        $totalPrice = $unitPrice * $quantity;
+
+        $output .='<tr>' .
+                        '<td>' . $result['purchaseID']. '</td>' . 
+                        '<td>' . $result['ItemNumber'] . '<td>' . 
+                        '<td>' . $result['purchaseDate'] . '</td>' . 
+                        '<td>' . $result['itemName'] . '</td>' . 
+                        '<td>' . $result['vendorName'] . '</td>' . 
+                        '<td>' . $result['quantity'] . '</td>' . 
+                        '<td>' . $resuult['unitPrice'] . '</td>' . 
+                        '<td>' . $totalPrice '</td>' . '</td>' . 
+                    '<tr>' ;
+    }
+
+    $purchaseStatement->closeCursor();
+    $output .= '</tbody>
+                    <tfoot>
+                        <tr>
+                             <th>Total</th>
+                             <th></th>
+                             <th></th>
+                             <th></th>
+                             <th></th>
+                             <th></th>
+                             <th></th>
+                             <th></th>
+                             <th></th>
+                        </tr>
+                    </tfoot>
+                </table>';
+    echo $output;
 ?>
