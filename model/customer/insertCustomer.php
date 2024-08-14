20344126d3222d47d72b676d86f0b789a6140562
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-08-14 09:14:48 -0700

    Add files via upload

--- model/customer/insertCustomer.php
+++ model/customer/insertCustomer.php
@@ -1,87 +1,85 @@
 <?php
-    require_once('../define/config/constant.php');
-    require_once('../define/config/dbconnect.php');
+    require_once('../../define/config/constants.php');
+    require_once('../../define/config/dbConnect.php');
 
-    if(isset($_POST['customerFullName']))
-    {
+    if(isset($_POST['customerFullName'])) {
         $custFullName = htmlentities($_POST['customerFullName']);
-        $custEmail = htmlenttiies($_POST['customerEmail']);
+        $custEmail = htmlentities($_POST['customerEmail']);
         $custMobile = htmlentities($_POST['customerMobile']);
         $custPhone = htmlentities($_POST['customerPhone']);
         $custAddress = htmlentities($_POST['customerAddress']);
         $custAddress2 = htmlentities($_POST['customerAddress2']);
         $custCity = htmlentities($_POST['customerCity']);
-        $custDistrict = htmlentities($_POST['customerStatus']);
+        $custDistrict = htmlentities($_POST['customerDistrict']);
+        $custStatus = htmlentities($_POST['customerStatus']);
 
-        if(isset($custFullName) && isset($custMobile) && isset($custAddress)){
-            if(filter_var($custMobile, FILTER_VALIDATE_INT) === 0 || filter_var($custMobile, FILTER_VALIDATE_INT)){
-                /* Valid mobile number */
+        if(isset($custFullName) && isset($custMobile) && isset($custAddress)) {
+            /* validate mobile number */
+            if(filter_var($custMobile, FILTER_VALIDATE_INT) === 0 || filter_var($custMobile, FILTER_VALIDATE_INT)) {
+                /* valid mobile */
             } else {
-                echo '<div class="alert alert-danger">
-                           <button type="close" data-dismiss="alert">&time;</button>
-                                 enter mobile number
+                /* invalid mobile */
+                echo '<div class="alert alert-danger"<button type="button" class="close" data-dismiss="alert">&times;</button>
+                           valid mobile phone number
                      </div>';
-                exit();
-            }        
-            /* validate phone number */
-            if(!empty($custPhone)){
-                if(filter_var($custPhone)){
-                    /* phone number valid */
-                    echo '<div class="alert alert-danger">
-                            <button type="button" class="close" data-dismiss="alert">&time;</button> 
-                                    enter phone number
-                        </div>';
+                exit();                     
+            }
+
+            /* validate second phone number  */
+            if(!empty($custPhone)) {
+                if(filter_var($custPhone, FILTER_VALIDATE_INT) === false ) {
+                    /* invalid phone number */
+                    echo '<div class="alert alert-danger"<button type="button" class="close" data-dismiss="alert">&times;</button>
+                               Enter valid phone number 
+                         </div>';
+                }
+            }
+
+            /* validate email */
+            if(!empty($custEmail)) {
+                if(filter_var($custEmail, FILTER_VALIDATE_EMAIL) === false) {
+                    /* invalid Email */
+                    echo '<div class="alert alert-danger"<button type="button" class="close" data-dismiss="alert">&times;</button>
+                              Enter valid email 
+                          </div>';
                     exit();
                 }
             }
 
-            /* validate email  */
-            if(!empty($custEmail)){
-                if(filter_var($custEmail, FILTER_VALIDATE_EMAIL) === false ){
-                    /* Email valid */
-                    echo '<div class="alert alert-danger"><button type="button" class="close" data-dimiss="alert">&times;</button>
-                            enter valid
-                        </div>';
-                    exit();
-                }
-            }
             /* validate address */
             if($custAddress == ''){
-                echo '<div class="alert alert-danger">
-                        <button type="button" class="close" data-dismiss="alert">&times;</button>
-                        Enter Address
-                    </div>';
-                exit();
-            }
-            /* validate full name */
-            if($custFullName == ''){
-                echo '<div class="alert alert-danger">
-                        <button type="button" class="close" data-dimiss="alert">&times</button>
-                                Enter Full Name
-                    </div>';
+                /* address is empty */
+                echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>
+                           Enter address 
+                      </div>';
                 exit();
             }
 
-        /* insert data */
-        $addCust = "INSERT INTO customer(fullName, email, mobile, phone, address, address2, city, district, status)
-                    VALUES('$custFullName','$custEmail','$custMobile','$custAddress','$custAddress','$custAddress2','$custDistrict','$custStatus')";
-        $custStatement = $deal->prepare($addCust);
-        $custStatement->execute(['fullName' => $custFullName,
-                                 'email' => $custEmail, 
-                                 'mobile' => $custMobile, 
-                                 'phone' => $custPhone,
-                                 'address' => $custAddress,
-                                 'address2' => $custAddress2,
-                                 'city' => $custCity,
-                                 'disstrict' => $custDistrict,
-                                 'status' => $custStatus ]);
-        echo '<div class="alert alert-success">
-                  <button type="button" class="close" data-dismiss="alert">&tims;</button>add customer to database</div>';
-        } else {
-            /* empty field */
-            echo '<div class="alert alert-success">
-                    <button type="button" class="close" data-dismiss="alert">&times;</button> 
-                            enter madatory field
-                </div>';
+            /* check full name is empty */
+            if($custFullName == '') {
+                /* full name is empty */
+                echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>
+                           Enter Full Name.
+                     </div>';
+                exit();
+            }
+
+            /* add customer record */
+            $addCust = "INSERT INTO customer(fullname, email, mobile, phone, address, address2, city, district, status)
+                        VALUES('$custFullName','$custEmail','$custMobile','$custPhone','$custAddress','$custAddress2','$custCity'$custDistrict','$custStatus')";
+            $custStatement = $deal->prepare($addCust);
+            $custStatement->execute(['fullName'=> $fullName, 'email'=> $custEmail,'mobile'=> $custMobile,'phone'=> $custPhone,'address'=> $custAddress,'address2'=>$custAddress,'city'=> $custCity,'district'=> $custDistrict, 'status'=> $custStatus]);
+
+            echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>
+                       Customer add to DB.
+                 </div>';
             exit();
+        } else {
+            /* mandatory fields */
+            echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>
+                       Enter all fields marked with a(*)
+                  </div>';
+            exit();
+        }
     }
+?>
