eac462ec024129d380294c8be5aa81267448197b
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-08-15 05:40:35 -0700

    Add files via upload

--- model/vendor/deleteVendor.php
+++ model/vendor/deleteVendor.php
@@ -1,51 +1,44 @@
 <?php
-	require_once('../../define/config/constants.php');
-	require_once('../../define/config/dbconnect.php');
-	
-	if(isset($_POST['vendorID'])){
-		
-		$vendorID = htmlentities($_POST['vendorID']);
-		/* check madatory fields */
-		if(!empty($vendorID)){
-			
-			/* Sanitize VendorID */
-			$vendorID = filter_var($vendorID, FILTER_SANITIZE_STRING);
+    require_once('../../define/config/constant.php');
+    require_once('../../define/config/dbConnect.php');
 
-			/* get vendor object from DB */
-			$qVen = "SELECT vendorID FROM vendor WHERE vendorID='$vendorID'";
-			$venStatement = $dbcon->prepare($qVen);
-			$venStatement->execute(['vendorID' => $vendorID]);
-			
-			if($venStatement->rowCount() > 0){
-				
-				/* Vendor exists in database, start delete process */
-				$delVen = "DELETE FROM vendor WHERE vendorID='$vendorID'";
-				$delVenStatement = $dbcon->prepare($delVen);
-				$delVenStatement->execute(['vendorID' => $vendorID]);
+    $vendorID = htmlentities($_POST['vendorID']);
 
-				echo '<div class="alert alert-success">
-				           <button type="button" class="close" data-dismiss="alert">&times;</button>
-					       Vendor deleted.
-					 </div>';
-				exit();
-				
-			} else {
-				
-				/* vendor does not exist */
-				echo '<div class="alert alert-danger">
-				           <button type="button" class="close" data-dismiss="alert">&times;</button>
-						   Vendor does not exist in DB. Therefore, can\'t delete.
-					  </div>';
-				exit();
-			}
-			
-		} else {
-			/* vendorID empty */
-			echo '<div class="alert alert-danger">
-			           <button type="button" class="close" data-dismiss="alert">&times;</button>
-					   Please enter the Vendor ID
-				  </div>';
-			exit();
-		}
-	}
+    if(isset($_POST['vendorID'])) {
+
+        /* check mandatory field */
+        if(!empty($vendorID)) {
+
+            /* Sanitize vendor ID */
+            $vendorID = filter_var($vendorID , FILTER_SANITIZE_STRING);
+
+            /* check vendorID exist in DB */
+            $qVen = "SELECT * FROM vendor WHERE vendorID = '$vendorID' ";
+            $venStatement = $deal->prepare($qVen);
+            $venStatement->execute(['vendorID'=> $vendorID]);
+
+            if($venStatement->rowCount() > 0) {
+
+                /* vendor exist in DB */
+                $delVen = "DELETE FROM vendor WHERE vendorID = '$vendorID' " ;
+                $delVenStatement = $deal->prepare($delVen);
+                $delVenStatement->execute(['vendorID'=> $vendorID ]);
+
+                echo '<div class="alert alert-succss"><button type="button" class="close" data-dismiss="alert">&times;</button> 
+                          Vendor deleted.</div>';
+                exit();
+            } else {
+
+                /* vendor does not exist */
+                echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button> 
+                           Vendor does not exist in DB.</div>';
+                exit();
+            }
+        } else {
+            /* Vendor is empty */
+            echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>
+                      Enter vendor.</div>';
+            exit();
+        }
+    }
 ?>
