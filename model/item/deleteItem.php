8fef9bbe3006c5f53f353546c383abc77f0c1c6a
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-08-15 04:51:14 -0700

    Add files via upload

--- model/item/deleteItem.php
+++ model/item/deleteItem.php
@@ -1,51 +1,43 @@
 <?php
-	require_once('../../define/config/constants.php');
-	require_once('../../define/config/dbconnect.php');
-	
-	$itemNumber = htmlentities($_POST['itemNumber']);
-	
-	if(isset($_POST['itemNumber'])){
-		
-		/* Check if mandatory fields are not empty  */
-		if(!empty($itemNumber)){
-			
-		
-			/*  Sanitize item number */
-			$itemNumber = filter_var($itemNumber, FILTER_SANITIZE_STRING);
+    require_once('../../define/config/constant.php');
+    require_once('../../define/config/dbConnect.php');
 
-			/* get item object from database */
-			$qItem = "SELECT itemNumber FROM item WHERE itemNumber='$itemNumber'";
-			$itemStatement = $conn->prepare($queryItem);
-			$itemStatement->execute(['itemNumber' => $itemNumber]);
-			
-			if($itemStatement->rowCount() > 0){
-		
-				/* item exists in database, start delete process */
-				$delItem = "DELETE FROM item WHERE itemNumber='$itemNumber'";
-				$delStatement = $dbcon->prepare($delItem);
-				$delStatement->execute(['itemNumber' => $itemNumber]);
+    $itemNumber = htmlentities($_POST['itemNumber']);
 
-				echo '<div class="alert alert-success">
-				          <button type="button" class="close" data-dismiss="alert">&times;</button>
-					  Item deleted.</div>';
-				exit();
-				
-			} else {
-				
-				echo '<div class="alert alert-danger">
-						   <button type="button" class="close" data-dismiss="alert">&times;</button>
-							Item does not exist in DB. Therefore, can\'t delete.
-					  </div>';
-				exit();
-			}
-			
-		} else {
-			
-			echo '<div class="alert alert-danger">
-			           <button type="button" class="close" data-dismiss="alert">&times;</button>
-					   Please enter the item number
-				  </div>';
-			exit();
-		}
-	}
+    if(isset($_POST['itemNumber'])) {
+
+        /* check mandatory fields */
+        if(!empty($itemNumber)) {
+
+            /* Sanitize item number */
+            $itemNumber = filter_var($itemNubmer, FILTER_SANITIZE_STRING);
+
+            /* check itemNumber exist in DB */
+            $qItem = "SELECT itemNumber FROM item WHERE itemNumber = '$itemNumber' ";
+            $itemStatement = $deal->prepare($qItem);
+            $itemStatement->execute(['itemNumber'=> $itemNumber]);
+
+            if($itemNumber->rowCount() > 0) {
+
+                /* item exist in DB */
+                $delItem = "DELETE FROM item WHERE itemNubmer = '$itemNumber' ";
+                $delItemStatement = $deal->prepare($delItem);
+                $delItemStatement->execute(['itemNumber'=> $itemNumber]);
+
+                    echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>
+                                Item deleted.</div>';
+                    exit();
+                } else {
+                    /* item does not exist */
+                    echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>
+                               Item does not exist in DB.</div>';
+                    exit();
+                }
+            } else {
+                /* itemNumber is empty */
+                echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times</button>
+                           Enter the item number</div>';
+                exit();
+            }
+    }
 ?>
