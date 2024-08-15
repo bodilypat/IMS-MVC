021eb5fcf9d3ec2a974e7512a24cc963f8292295
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-08-15 05:07:25 -0700

    Add files via upload

--- model/customer/deleteCustomer.php
+++ model/customer/deleteCustomer.php
@@ -1,51 +1,45 @@
 <?php
-	require_once('../../define/config/constants.php');
-	require_once('../../define/config/dbconnect.php');
-	
-	if(isset($_POST['customerID'])){
-		
-		$customerID = htmlentities($_POST['customerID']);
-		
-		/* check mandatory filed */
-		if(!empty($customerD)){
-			
-			/* sanitize customerID */
-			$customerID = filter_var($customerID, FILTER_SANITIZE_STRING);
+    require_once('../../define/config/constant.php');
+    require_once('../../define/config/dbConnect.php');
 
-			/* get customer object in database */
-			$qCust = "SELECT customerID FROM customer WHERE customerID= '$customerID'";
-			$custStatement = $dbcon->prepare($qCust);
-			$custStatement->execute(['customerID' => $customerID]);
-			
-			if($custStatement->rowCount() > 0){
-				
-				/* customse exist in database,  star delete  process */
-				$delCust = " DELETE FROM customer WHERE customerID= '$customerID'";
-				$delStatement = $dbcon->prepare($delCust);
-				$delStatement->execute(['customerID' => $customerID]);
+    if(isset($_POST['customerID'])) {
 
-				echo '<div class="alert alert-success">
-				          <button type="button" class="close" data-dismiss="alert">&times;</button>
-						  Customer deleted.
-					  </div>';
-				exit();
-				
-			} else {
-				/* customer does not exist */
-				echo '<div class="alert alert-danger">
-				           <button type="button" class="close" data-dismiss="alert">&times;</button>
-					       Customer does not exist in DB. Therefore, can\'t delete.
-					  </div>';
-				exit();
-			}
-			
-		} else {
-			
-			echo '<div class="alert alert-danger">
-			           <button type="button" class="close" data-dismiss="alert">&times;</button>
-					   Please enter the CustomerID
-				 </div>';
-			exit();
-		}
-	}
+        $customerID = htmlentities($_POST['customerID']);
+
+        /* check mandatory fields */
+        if(!empty($customerID)) {
+            
+            /* Sanitize field */
+            $customer = filter_var($customerID, FILTER_SANITIZE_STRING);
+
+            /* check customerID is exist in DB */
+            $qCust = "SELECT customerID FROME customer WHERE customerID ='$customerID ' ";
+            $custStatement = $deal->prepare($qCust);
+            $custStatement->execute(['customerID'=> $customerID ]);
+
+            if($custStaement->rowCount() > 0) {
+
+                /* customer exist in DB */
+                $delCust = "DELETE FROM customer WHERE customerID = '$customerID' ";
+                $delCustStatement = $deal->prepare($delCust);
+                $delCustStatement->execute(['customer'=> $customerID ]);
+
+                echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>
+                           customer deleted.</div>';
+                exit();
+            } else {
+
+                    /* customer does not exist */
+                echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>
+                           Customer does not exist in DB.</div>';
+                exit();
+            }
+        } else {
+
+            /* customerID is empty */
+            echo '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>
+                       Enter customer ID.</div>';
+            exit();
+        }
+    }
 ?>
