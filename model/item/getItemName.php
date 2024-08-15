ba3faf78cec2e97ba44e84afd9b118341d887164
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-08-15 06:29:03 -0700

    Add files via upload

--- model/item/getItemName.php
+++ model/item/getItemName.php
@@ -1,21 +1,21 @@
 <?php
-	require_once('../../define/config/constants.php');
-	require_once('../../define/config/db.php');
+    require_once('../../define/config/constants.php');
+    require_once('../../define/config/dbConnect.php');
 
-	// Execute the script if the POST request is submitted
-	if(isset($_POST['itemNumber'])){
-		
-		$itemNumber = htmlentities($_POST['itemNumber']);
-		
-		$queryItem = "SELECT * FROM item WHERE itemNumber = '$itemNumber'";
-		$itemStatement = $dbcon->prepare($queryItem);
-		$itemStatement->execute(['itemNumber' => $itemNumber]);
-		
-		// get item object , return it as a json object
-		if($itemStatement->rowCount() > 0) {
-			$resultset = $itemStatement->fetch(PDO::FETCH_ASSOC);
-			echo json_encode($resultset);
-		}
-		$itemStatement->closeCursor();
-	}
+    $itemNumber = htmlentities($_POST['itemNumber']);
+    /* check POST request */
+    if(isset($_POST['itemNumber'])) {
+
+        /* Get ItemName */
+        $qItem = "SELECT * FROM item WHERE itemNumber = '$itemNumber' ";
+        $itemStatement = $deal->prepare($qItem);
+        $itemStatement->execute(['itemNumber'=> $itemNumber]);
+
+        /* found data given itemNumber, return it as a json objects */
+        if($itemStatement->rowCount() > 0) {
+            $result = $itemStatement->fetch(PDO::FETCH_ASSOC);
+            echo json_encode($result);
+        }
+        $itemStatement->closeCurSor();
+    }
 ?>
