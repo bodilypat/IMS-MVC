2b048c795432be5776f16c64179f5bca93f4b2b9
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-23 07:18:10 -0700

    Add files via upload

---
+++ assign/footer.php
@@ -0,0 +1,28 @@
+    <!-- Footer -->
+    <footer class="footer bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Inventory System <?php echo date('Y'); ?></p>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="enclose/js/jquery.min.js"></script>
+    <script src="enclose/js/bootstrap.bundle.min.js"></script>
+	
+	<!-- Datatables script -->
+	<script src="enclose/js/datatables.js" type="text/javascript" charset="utf8" ></script>
+	<script src="assign/js/sumsum.js" type="text/javascript" charset="utf8"></script>
+	
+	<!-- Chosen files for select boxes -->
+	<script src="enclose/js/chosen.jquery.min.js"></script>
+	<link href="assign/css/chosen.css" rel="stylesheet" />
+	
+	<!-- Datepicker JS -->
+	<script src="enclose/js/bootstrap-datepicker.min.js"></script>
+	
+	<!-- Bootbox JS -->
+	<script src="enclose/js/bootbox.min.js"></script>
+	
+	<!-- Custom scripts -->
+	<script src="assign/js/scripts.js"></script>
+	<script src="assign/js/login.js"></script>
