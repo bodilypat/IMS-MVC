c1c76e051d89eac4dc90ed0fa174990d7dc09d77
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-10-04 09:04:54 -0700

    Add files via upload

--- includes/functions.php
+++ includes/functions.php
@@ -4,8 +4,24 @@
 
     function addProduct($name, $description, $quantity,$price,$category_id){
         global $pdo;
-        $stmt = $pdo->prepare("INSERT INTO products (product_name, description, price, quantity, category_id) VALUES (?,?,?,?,?)");
-        return $stmt->execute([$product_name, $description, $price, $quantity, $category_id]);
+        $stmt = $pdo->prepare("INSERT INTO products (product_name, description, price, quantity, category_id,supplier_id) 
+                               VALUES ('$name','$description','$price','$quantity','$category_id','$supplier_id') ");
+        // Bind Parameters
+        $stmt->bindParam($name);
+        $stmt->bindParam($description);
+        $stmt->bindParam($price);
+        $stmt->bindParam($quantity);
+        $stmt->bindParam($category_id, PDO::PARAM_INT);
+        $stmt->bindParam($supplier_id, PDO::PARAM_INT);
+        
+        //Execute the statement
+        return $stmt->execute([$product_name, $description, $price, $quantity, $category_id,$supplier_id]);
+    }
+
+    function getProducts(){
+        global $pdo;
+        $stmt = $pdo->prepare("SELECT  * FROM products");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     function getProductById($product_id){
@@ -13,12 +29,6 @@
         $stmt = $pdo->prepare("SELECT * FROM products WHERE id = ? ");
         $stmt->execute([$product_id]);
         return $stml->fetch(PDO::FETCH_ASSOC);
-    }
-
-    function getProducts(){
-        global $pdo;
-        $stmt = $pdo->prepare("SELECT  * FROM products");
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
     
     function getProductByCID($category_id) {
@@ -57,3 +67,28 @@
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
     
+    function updateProduct($id,$name, $description, $price, $quantity, $category_id, $supplier_id){
+        global $pdo;
+        $stmt = $pdo->prepare("UPDATE products
+                               SET name ='$name', description = '$description', price = '$price', quantity = '$quantity', category_id = '$category_id', supplier_id = '$supplier_id'
+                               WHERE id ='$id' ");
+
+        //Bind parameter
+        $stmt->bindParam($name);
+        $stmt->bindParam($description);
+        $stmt->bindParam($price);
+        $stmt->bindParam($quantity);
+        $stmt->bindParam($categor_id, PDO::PARAM_INT);
+        $stmt->bindParam($suplier_id, PDO::PARAM_INT);
+        $stmt->bindParam($id, PDO::PARAM_INT);
+
+        //Execute the statement
+        return $stmt->execute();
+    }
+
+    function deleteProduct($id){
+        $stmt = $pdo->prepare("DELETE FROM products WHERE id = '$id' ");
+        $stmt->bindParam($id, PDO::PARAM_INT);
+
+        return $stmt->execute();
+    }
