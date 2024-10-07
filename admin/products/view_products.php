1e096c19e4f91b5be45c386a0f7d51b558cf8d87
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-10-07 04:54:43 -0700

    Add files via upload

--- admin/products/view_products.php
+++ admin/products/view_products.php
@@ -1,40 +1,48 @@
 <?php
 
-    require '../includes/functions.php';
+    include '../includes/functions.php';
+
     $products = getProducts();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
-        <title>Product Lists</title>
+        <meta charset="UTF-8">
+        <title>View Product</title>
+        <link rel="stylesheet" href="../assets/css/styels.css">
     </head>
     <body>
-        <table border="1" name="form-product">
-            <tr>
-                 <th>ID</th>
-                 <th>Product Name</th>
-                 <th>Description</th>
-                 <th>Quantity</th>
-                 <th>Price</th>
-                 <th>Actionts</th>
-            </tr>
-            <tbody>
-                <?php foreach($products as $product): ?>
-                <tr>
-                     <td><?php echo $product['id'];?></td>
-                     <td><?php echo htmlspecialchars($product['name']);?></td>
-                     <td><?php echo htmlspecialchars($product['desccription']);?></td>
-                     <td><?php echo htmlspecialchars($products['quantity']);?></td>
-                     <td><?php echo htmlspecialchars($product['price']);?></td>
-                     <td>
-                        <a href="edit_product.php?id=<?php echo $product['id'];?>">Edit</a>
-                        <a href="delete_product.php?id=<?php echo $product['id'];?>" onclick="Return confirm('Are you want to delete this product?');">Delete</a>
-                     </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-        <a href="add_product.php">Add New Product</a>
+        <div class="container">
+            <h1 class="mt-5">Product Record</h1>
+            <table class="table table-product">
+                <thead>
+                      <tr>
+                           <th>ID</th>
+                           <th>Name</th>
+                           <th>Description</th>
+                           <th>Price</th>
+                           <th>Stock</th>
+                           <th>Actions</th>
+                      </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($products as $product): ?>
+                      <tr>
+                           <td><?php htmlspecialchars($product['id']);?></td>
+                           <td><?php htmlspecialchars($product['name']);?></td>
+                           <td><?php htmlspecialchars($product['description']);?></td>
+                           <td><?php htmlspecialchars(number_format($product['price'], 2)); ?></td>
+                           <td><?php htmlspecialchars($product['stock']); ?></td>
+                           <td>
+                                <a href="edit_product.php?id=<?php $product['id']; ?>" class="btn btn-warning btn-sm">Edit</a>
+                                <a href="delete_product.php?id=<?php $product['id']; ?>" class="btn btn-danger btn-sm" onClick="return confirm('Are you sure?')">Delete</a> 
+                           </td>
+                      </tr>
+                      <?php endforeach; ?>
+                </tbody>
+            </table>
+            <a href="add_product.php" class="btn btn-primary">Add New Product</a>
+        </div>
     </body>
 </html>
