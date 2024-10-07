33ee2bf20463bb4db9a849e70b691b60eae9857b
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-10-07 05:04:03 -0700

    Add files via upload

--- database/db_inventory.sql
+++ database/db_inventory.sql
@@ -12,37 +12,64 @@ CREATE TABLE users(
 );
 
 CREATE TABLE suppliers(
-    supplier_id INT PRIMARY KEY,
-    name VARCHAR(255),
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
     contactInfo VARCHAR(255),
-    address VARCHAR(255),
+    phone VARCHAR(20),
+    email VARCHAR(100),
+    address TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE purchases(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT NOT NULL,
+    supplier_id INT NOT NULL,
+    quantity INT NOT NULL,
+    purchase_date  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    total_cost DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES  products(id),
+    FOREIGN KEY (supplier_id) REFERENCES suppliers(id)
 );
 
 CREATE TABLE categories(
-    category_id PRIMARY KEY,
+    id PRIMARY KEY,
     name VARCHAR(255),
-    description TEXT
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 
 CREATE TABLE products(
     product_id INT PRIMARY KEY,
-    description TEXT,
+    name VARCHAR(100) NOT NULL
     category_id INT,
-    price DECIMAL(10,2),
     supplier_id INT,
-    quantityInStock INT,
+    description TEXT,
+    price DECIMAL(10,2),
+    stock INT,
     recordLevel INT,
-    FOREIGN KEY (category_id) REFERENCES categories(category_id),
-    FOREIGN KEY (supplier_id) REFERENCES suppliers(supplier_id),
+    created_at TIMESTAMP CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    FOREIGN KEY (supplier_id) REFERENCES suppliers(id),
 );
 
-CREATE TABLE transections(
-    transection_id INT PRIMARY KEY,
-    product_id INT,
-    quantity INT,
-    transectionType ENUM('IN','OUT'),
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    user_id INT,
-    FOREIGN KEY (product_id) REFERENCES  products(product_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+
+CREATE TABLE sales(
+    id  INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    product_id INT NOT null,
+    quantity INT NOT NULL,
+    sale_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (customer_id) REFERENCES customers(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+CREATE TABLE customers(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
