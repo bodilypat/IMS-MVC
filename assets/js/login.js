3839a9b5e792c9e9b44d6a8ee202dfadc03350a3
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-03-12 08:46:02 -0700

    Add files via upload

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,101 +1,79 @@
 $(document).ready(function() {
-    /* list to register button */
+    /* Listen to register button */
     $('#register').on('click', function() {
         register();
     });
 
-    /* lister to reset password button */
+    /* Listen to reset password button */
     $('#resetPassword').on('click', function() {
         resetPassword();
     });
 
-    /* listen to login button */
+    /* Listen to login button */
     $('#login').on('click', function() {
         login();
     });
 });
+
 /* function to register a new user */
 function register() {
     var registFullName = $('#registFullName').val();
-    var registUsername = $('#registUsername').val();
-    var registPass = $('#registPassword').val();
-    var registCfPass = $('#registConfirmPassword').val();
+    var registUsername = $('#registUserName').val();
+    var registPassword = $('#registPassword').val();
+    var registCfpassword = $('#registCfpassword').val();
 
     $.ajax({
-        url: 'admin/login/register.php';
+        url : 'admin/login/register.php',
         method: 'POST',
-        data: {  /* data sent to the server */
+        data: {
             registFullName:registFullName,
             registUsername:registUsername,
-            registPass:registPass,
-            registCfPass:registCfPass,
-        }, 
-        success: function(data) { /* success callback */
-            $('#registMessage').html(data);
+            registPassword:registPassword,
+            registCfpassword:registCfpassword,
         },
-        error: function(xhr, status, error) { // Error callback in case of AJAX failure
-            console.log("Request failed:" + error);
-            $('#registMessage').html('An error occurred. Please try again later.');
+        success: function(data) {
+            $('registMessage').html(data);
         }
     });
 }
 
-/* function to reset password */
+/* Function to reset password */
 function resetPassword() {
     var resetUsername = $('#resetUsername').val();
-    var resetPassword = $('#resetPasswrod').val();
-    var confirmPassword = $('#confirmPassword').val();
+    var resetPassword = $('#resetPassword').val();
+    var resetCfpassword = $('#resetCfpassword').val();
 
-    /* Client - side validation for passwords */
-    if(resetPassword === '' || confirmPassword === '') {
-        $('#resetPasswordMessage').html('Please fill in both password fields.');
-        return; /* stop the function if validation fails */
-    }
-
-    if(resetPassword !== confirmPassword) {
-        $('#resetPasswordMessage').html('Please fill in both password fields.');
-        return; /* Stop the function if validation fails */
-    }
-    
     $.ajax({
-        url: 'admin/login/resetPassword.php',
+        url: 'model/login/resetPassword.php',
         method: 'POST',
         data: {
             resetUsername:resetUsername,
             resetPassword:resetPassword,
-            confirmPassword:confirmPassword,
+            resetCfpassword:resetCfpassword,
         },
         success: function(data) {
             $('#resetPasswordMessage').html(data);
-        },
-        error: function(xhr, status, error) {
-            console.log('request failed:' + error);
-            $('#resetPassordMessage').html('An error occcurred. Plase try-again later.');
         }
     });
 }
 
-/* function login a user */
+/* Function to login a user */
 function login() {
     var loginUsername = $('#loginUsername').val();
     var loginPassword = $('#loginPassword').val();
 
     $.ajax({
-        url: 'admin/login/checkLogin.php',
+        url: 'model/login/checkLogin.php',
         method: 'POST',
-        data : {
+        data: {
             loginUsername:loginUsername,
             loginPassword:loginPassword,
         },
         success: function(data) {
             $('#loginMessage').html(data);
-            if (data.indexOf('Redirecting') >= 0) {
-                window.location = 'index.php';
+            if(data.indexOf('redirecting') >= 0) {
+                window.location = 'index.php'
             }
-        },
-        error: function(xhr, status, error) {
-            console.log('request failed: ' + error);
-            $('#loginMessage').html("An error occurred. Please try again later.");
         }
     });
 }
