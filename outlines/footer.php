9603d23b0e702f19196b764f84e6115a53588977
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-03-12 09:21:52 -0700

    Add files via upload

---
+++ outlines/footer.php
@@ -0,0 +1,21 @@
+<!-- Footer -->
+<footer class="footer bg-dark">
+      <div class="container">
+            <p class="m-0 text-center text-white">Inventory System<?php echo date('Y');?></p>
+      </div>
+</footer>
+<script src="compose/boostrap/jquery.min.js"></script>
+<script src="compose/boostrap/js/bootstrap.bundle.min.js"></script>
+
+<script type="text/javascript" chaset="utf8" src="compose/dataTables/js/datatables.js"></script>
+<script type="text/javascript" chaset="utf8" src="compose/dataTables/js/sumsum.js"></script>
+
+<script src="compose/datapicker/js/boostrap-datepicker.min.js"></script>
+
+<script src="compose/chosen/js/chosen.jquery.min.js"></script>
+<script  rel="stylesheet" src="compose/chosen/css/chosen.css"></script>
+
+<script src="compose/bootbox/js/bootbox.min.js"></script>
+
+<script scr="compose/custom/js/script.js"></script>
+<script src="compose/custom/js/login.js"></script>
