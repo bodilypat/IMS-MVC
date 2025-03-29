db23c87913f11a2ef290c4d649c185d57c13f8b6
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-03-29 13:27:42 -0700

    Update view_customers.php

--- admin/customers/view_customers.php
+++ admin/customers/view_customers.php
@@ -1,7 +1,8 @@
 <?php
-    require '../incldues/functions.php';
-
-    $customers = getCustomers();
+    require '../incldues/dbconnect.php';
+    $qCust = "SELECT * FROM customers;
+    $result = $db_con->query($qCust);
+    $customers = $db_con->fetch_assoc($result)
 ?>
 
 <!DOCTYPE html>
@@ -17,16 +18,30 @@
                   <th>ID</th>
                   <th>Name</th>
                   <th>Email</th>
+                  <th>Mobile</th>
                   <th>Phone</th>
                   <th>Address</th>
+                  <th>City></th>
+                  <th>State</th>
+                  <th>Zipcode</th>
+                  <th>Status</th>
+                  <th>Create On</th>
             </tr>
             <?php foreach($customers as $customer): ?>
             <tr>
                   <td><?php $customer['id'];?></td>
                   <td><?php $customer['name'];?></td>
                   <td><?php $customer['email'];?></td>
+                  <td><?php $customer['mobile'];?></td>
                   <td><?php $customer['phone'];?></td>
                   <td><?php $customer['address'];?></td>
+                  <td><?php $custoemr['city'];?></td>
+                  <td><?php $customer['state'];?></td>
+                  <td><?php $customer['zipcode'];?></td>
+                  <td><?php $customer['createdOn'];?</td>
+                  <td><a href='update_customer.php?id=" . $customer['customer_id'] . "'>Edit</a> |
+                      <a href='delete_customer.php?id=" . $customer['customer_id'] . "'>Delete</a>
+                  </td>
             </tr>
             <?php endforeach; ?>
         </table>
