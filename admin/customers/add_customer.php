2a0494e514854e8152a3e403c2c5eec2dfbba552
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-03-29 12:40:55 -0700

    Update add_customer.php

--- admin/customers/add_customer.php
+++ admin/customers/add_customer.php
@@ -1,18 +1,26 @@
 <?php
 
-    require '../includes/functions.php';
+    require '../includes/dbconnect.php';
 
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
-        $name = $_POST['name'];
+        $full_name = $_POST['full_name'];
         $email = $_POST['email'];
+        $mobile = $_POST['mobile'];
         $phone = $_POST['phone'];
         $address = $_POST['address'];
+        $city = $_POST['city'];
+        $state = $_POST['state'];
+        $status = $_POST['status'];
 
-        if(addCustomer($name, $email, $phone, $address)) {
+        $sql_cust = "INSERT INTO customers(full_name, email,mobile, phone, address, city, state, status)
+                     VALUES('$full_name','$email','$mobile','$phone','$address','$city','$state','$status')";
+        
+        if($db_con->qurey($sql-cust) === TRUE) {
             echo "Customer added successfull!";
         } else {
-            echo "Failed to add customer.";
+            echo "Error: " . $sql . "<br>" .$db_con->error;
         }
+        $db_con->close();
     }
 ?>
 
@@ -31,13 +39,36 @@
                 <input type="email" name="email" class="form-group" placeholder="Customer Email" required>
             </div>
             <div class="form-group">
+                <label for="mobile">Mobile: </label>label>
+                <input type="tel"  id="mobile" name="mobile" placeholder="Customer Mobile" required>
+            </div>
+            <div class="form-group">
                 <label for="Phone">Phone</label>
-                <input type="text" name="phone" class="form-control" placeholder="Contact Number" required>
+                <input type="text" name="phone" class="form-control" placeholder="Contact Phone" required>
             </div>
             <div class="form-group">
                 <label for="Address">Address</label>
                 <textarea name="address" class="form-control" placeholder="Customer Address" required>
             </div>
+            <div class="form-group">
+                    <label for="city">City</label>
+                    <input type="text" id ="name" name="city" placeholder="Customer City" required>
+            </div>
+            <div class="form-group">
+                    <label for="state">State: </label>
+                    <input type="text" id="state" name="state" placeholder="Customer State" required>
+            </div>
+            <div class="form-group">
+                    <label for="zipcode">Zipcode: </label>
+                    <input type="text" id="zipcode" name="zipcode" placeholder="Customer Zipcode" required>
+            </div>
+            <div class="status">
+                  <label for="status">Status: </label>
+                  <select name="status">
+                        <option value="Active">Active</option>
+                        <option value="Inactive">Inactive</option>
+                  </select>
+                </div>
             <button type="submit" name="addCust" value="add customer" >Add Customer</button>
         </form>
         <a href="view_customers.php"></a>
