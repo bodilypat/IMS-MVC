33f5af25230bbc32f7527e9671e803a9bf9cc8a6
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-03-31 12:05:34 -0700

    Update update_order.php

--- admin/orders/update_order.php
+++ admin/orders/update_order.php
@@ -5,15 +5,15 @@
          $order_id = $_GET['id'];
 
          /* Fetch order details from database  */
-         $sql = "SELECT * FROM orders WHERE order_id = $order_id";
-         $result = $db_con->query($sql);
-
-         if ($result->num_rows == 1) {
-              $row = $result->fetch_assoc();
-         } else {
-              echo " Order not found";
-              exit;
-         }
+         $qOrder = "SELECT o.order_id, o.order_date, o.discount, o.qunaity, o.unit_price, o.total_price, ,o.status, i.item_name, c.customer_name
+                         FROM orders o 
+                         JOIN items i ON o.item_id = i.item_id
+                         JOIN customers c ON o.customer_id = c.customer_id";
+                    WHERE order_id = '$order_id' ";
+          
+          $stmt = $db_con->query($qOrder);
+          $orders = $stmt->fetchAll();
+         
      }
      if ($_SERVER['REQUEST_MOTHOD'] == 'POST' && isset($_POST['id'])) {
          $order_id = $_POST['id'];
@@ -23,6 +23,7 @@
          $discount = $_POST['discount'];
          $quantity = $_POST['quantity'];
          $unit_price = $_POST['unit_price'];
+         $total_price = $_POST['total_price'];
          $status = $_POST['status'];
 
          /* Calculate new total price  */
@@ -51,34 +52,43 @@
      </div>
      <div class="form-group">
          <label for="customer-id">Customer ID : </label>
-         <input type="number" name="customer_id" value="<?php echo $row['customer_id']; ?>" required>
+         <select name="customer_id" required>
+              <?php foreach($orders as $order): ?>
+                  <option value="<?php echo $order['customer_id'];?>"><?php echo $order['customer_name'];?></option>
+               <?php endforeach;  ?>
+         </select>
      </div>
      <div class="form-group">
          <label for="item-id">Item ID: </label>
-         <input type="number" name="item_id" value="<?php ech $row['item_id"]; ?>" required>
+         <select name="item_id" requried>
+              <?php foreach($orders as $order): ?>
+                  <option value="<?php echo $orders['item_id'];?>"><?php echo $order['item_name'];?></option>
+              <?php foreach; ?>
+         </select>
+          
      </div>
      <div class="form-group">
          <label for="order-date">Order Date: </label>
-         <input type="date" name="order_date" value="<?php echo $row['order_date'];?>" required>
+         <input type="date" name="order_date" value="<?php echo $orders['order_date'];?>" required>
      </div>
      <div class="form-group">
          <label for="discount">Discount: </label>
-         <input type="number" step="0.01" name="discount" value="<?php echo $row['quantity'];?>" required>
+         <input type="number" step="0.01" name="discount" value="<?php echo $orders['quantity'];?>" required>
      </div>
      <div class="form-group">
          <label for="quantity">Quantity: </label>
-         <input type="number" name="quantity" value="<?php echo $row['quantity'];?>" required>
+         <input type="number" name="quantity" value="<?php echo $orders['quantity'];?>" required>
       </div>
       <div class="form-group">
           <label for="unit-price">Unit Price: </label>
-          <input type="number" step="0.01" name="unit_price" value="<?phpe echo $row['unit_price'];?>" required>
+          <input type="number" step="0.01" name="unit_price" value="<?phpe echo $orders['unit_price'];?>" required>
       </div>
       <div class="form-group">
            <label for="status">Status: </label>
            <select name="status">
-                <option value="Pending" <?php if ($row['status'] == 'Pending') echo 'selected';? >>Pending</option>
-                <option value="Completed" <?php if ($row['status'] == 'Completed') echo 'selected'; ?>>Completed</option>
-                <option value="Cancelled" <?php if ($row['status'] == 'Cancelled') echo 'selected'; ?>>Cancelled</option>
+                <option value="Pending" <?php if ($orders['status'] == 'Pending') echo 'selected';? >>Pending</option>
+                <option value="Completed" <?php if ($orders['status'] == 'Completed') echo 'selected'; ?>>Completed</option>
+                <option value="Cancelled" <?php if ($orders['status'] == 'Cancelled') echo 'selected'; ?>>Cancelled</option>
            </select>
       </div>
       <button type="submit" name="update_order" value="update Order">Update Order</button>
