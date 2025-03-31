8338dca27abfdb85d7fd2dbd23d0a03b92a2f92c
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-03-31 11:12:07 -0700

    Update delete_purchase.php

--- admin/purchases/delete_purchase.php
+++ admin/purchases/delete_purchase.php
@@ -1,9 +1,12 @@
 <?php
 
-    require '../includes/functions.php';
+    include('../includes/dbconnect.php';
 
-    if(isset($_GET['id'])) {
-        deletePurchase($_GET['id']);
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $purchase_id = $_POST['purcahse_id'];
+        $sql ="DELETE FROM purchases WEHRE purchase_id = '$purchase_id'" ;
+        $stmt = $db_con->query($sql);
+        
         header("Location: manage_purchases.php");
         exit();
     } else {
