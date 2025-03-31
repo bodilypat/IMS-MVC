29a8c7f193c535c26202f127df916d7202229489
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-03-31 11:39:20 -0700

    Update add_order.php

--- admin/orders/add_order.php
+++ admin/orders/add_order.php
@@ -1,21 +1,29 @@
 <?php
     include('../include/dbconnect.php');
+    if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['id'])) {
+        $order_id = $_GET['id'];
+
+        /* Retrieve Data From Database  */
+        $qOrder ="SELECT o.order_id, o.order_date, o.discount, o.quantity, o.unit_price, o.totol_price, o.status, i.item_name, c.customer_name
+              FROM order o
+              JOIN items i ON o.item_id = i.item_id
+              JOIN customer c ON o.customer_id = c.customer_id";
+        $stmt = $db_con->query($qOrder);
+        $orders = $stmt->fetchAll();
+    }
 
     if ($_SERVER['REQUEST_METHOD'] =='POST') {
-        $customer_id = $_POST['customer_id'];
         $item_id = $_POST['item_id'];
+        $customer_id = $_POST['customer_id'];
         $order_date = $_POST['order_date'];
         $discount = $_POST['discount'];
         $quanity = $_POST['quantity'];
         $unit_price = $_POST['unit_price'];
         $status = $_POST['status'];
 
-        // Caculate total price 
-        $total_price = ($quantity * $unit_price) -  $discount;
-
       /* Insert the order into the database  */
       $sql = "INSERT INTO orders(customer_id, item_id, order_date, discount, quantity, unit_price, total_price, status)
-              VALUES('$customer_id','$item_id','$order_date','$discount','$quantity','$unit_price','$total_price','$status')";
+              VALUES('$customer_id','$item_id','$order_date','$discount','$quantity','$unit_price','$status')";
       if ($db_con->query($sql) === TRUE) {
           echo "Add new order successfully.";
       } else {
@@ -27,11 +35,19 @@
 <form method="POST" action="create_order.php">
     <div class="form-group">
           <label for="customer-id">Customer ID :</label>
-          <input type="number" id="customer_id" name="customer_id" required>
+          <select name="customer_id" required>
+              <?php foreach($orders as $order): ?>
+                  <option value="<?php echo $order['customer_id'];?>"><?php $order['customer_name']; ?></option>
+              <?php endforeach; ?>
+          </select>
     </div>
     <div class="form-group">
           <label for="Item-id">Item ID : </label>
-          <input type="number" id="item_id" name="item_id" required>
+          <select name="item_id" requried>
+              <?php foreach($orders as $order): ?>
+                   <option value="<?php echo $order['item_id'];?>"><?php echo $order['item_name']; ?></option>
+              <?php endforeach; ?>
+          </select>
     </div>
     <div class="form-group">
           <label for="order-date">Order Date: </label>
