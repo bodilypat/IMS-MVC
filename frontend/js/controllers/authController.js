f6bbdca29bf8da53ca9630c4f6907b90d7cc9caf
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-10-30 10:46:32 -0700

    Create authController.js for authentication logic

---
+++ frontend/js/controllers/authController.js
@@ -0,0 +1,8 @@
+// Frontend/js/controllers/authController.js
+
+import { AuthModel } from '../models/authModel.js';
+import { loginView } from '../views/loginView.js';
+
+export const AuthController = {
+
+};
